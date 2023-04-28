7f03db3f583d3fd9aaa328adc48a76137dd34829
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-04-28 09:18:01 -0300

    Subiendo ejercicios

---
+++ ejercicio-1.js
@@ -0,0 +1,49 @@
+
+//1. Escribir un programa que lea un número entero y determine si es par o impar.
+/*
+export default function numeroParImpar(numero,btnParImpar){
+    
+    let resultado = "";
+
+    const $btn = document.querySelector(btnParImpar);
+    const $numero = document.querySelector(numero).value;
+
+
+    $btn.addEventListener("click", (e) => {
+        
+        if(e.target === btnParImpar){
+            
+            if(($numero % 2) === 0){
+                resultado = "par";
+            }else{
+                resultado = "impar"; 
+            }
+        
+        }
+        console.log(resultado);
+
+    });
+
+}*/
+
+export default function numeroParImpar(numero, btnParImpar, resultadoParImpar) {
+    const $btn = document.querySelector(btnParImpar);
+
+    const $resultadoParImpar = document.querySelector(resultadoParImpar);
+
+    $btn.addEventListener("click", () => {
+      
+        const $numero = parseInt(document.querySelector(numero).value);
+        let resultado = "";
+      
+        if($numero % 2 === 0){
+            resultado = "par";
+        }else{
+            resultado = "impar";
+        }
+
+        console.log(resultado);
+        $resultadoParImpar.textContent = `El numero ${$numero} es: ${resultado}`;
+    });
+  
+}
---
+++ index.js
@@ -0,0 +1,7 @@
+import numeroParImpar from "./ejercicio-1.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    numeroParImpar("#numero-par-impar", "#btn-par-impar", ".resultado-par-impar");
+
+});
