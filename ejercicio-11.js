2c89a37c05b0a033138de81fab25abac4d2307be
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-11 21:31:44 -0300

    Agregando ej 11

--- ejercicio-11.js
+++ ejercicio-11.js
@@ -4,16 +4,29 @@
 
 export default function contarLetras(palabra, btn, resultado){
 
-    const $palabra = document.querySelector(palabra);
 
     const $btnLetras = document.querySelector(btn);
 
     const $resultado = document.querySelector(resultado);
 
+    let contador = 0;
+
     $btnLetras.addEventListener("click", () => {
 
-        /* asdasd */
+        const $palabra = document.querySelector(palabra).value;
+        const arrayPalabra = $palabra.split('');
 
+        for(let i = 0; i < arrayPalabra.lenght; i++ ){
+            for(let j = 1; j < arrayPalabra.lenght; j++){
+                if(arrayPalabra[i] === arrayPalabra[j]){
+                    contador++;
+                }
+            }
+        }
+
+        $resultado.textContent = `La palabra tiene ${contador} letras repetidas`;
+
+     
     });
 
 
