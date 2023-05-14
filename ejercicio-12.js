bb789f686b9dbc5f5d2ee5b02b48a64e52636e83
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-14 00:22:48 -0300

    Documentando ejercicio 12

--- ejercicio-12.js
+++ ejercicio-12.js
@@ -4,6 +4,7 @@
 
 export default function contarMayorMenor(array, btnMayor, btnMenor, resultado){
 
+    // Declarando variables de DOM y variable del array
     const $array = document.querySelector(array);
 
     const arrayNuevo = [23,56,83,3,89,14,1];
@@ -12,12 +13,14 @@ export default function contarMayorMenor(array, btnMayor, btnMenor, resultado){
 
     const $resultado = document.querySelector(resultado);
 
+    //Detecta el evento click en el DOM
     document.addEventListener("click", (e) => {
 
+        //Declarando variables
         let mayor = arrayNuevo[0];
         let menor = arrayNuevo[0];
 
-
+        //Matchea si el evento click fue del boton de mayor
         if(e.target.matches(btnMayor)){
             for(let i = 0; i < arrayNuevo.length; i++){
                 if(mayor < arrayNuevo[i]){
@@ -25,10 +28,12 @@ export default function contarMayorMenor(array, btnMayor, btnMenor, resultado){
                 }
             }
 
+            //Muestra el resultado en el span
             $resultado.textContent = ` El mayor es: ${mayor}`;
 
         }
 
+        //Matchea si el evento click fue del boton menor
         if(e.target.matches(btnMenor)){
             for(let i = 0; i < arrayNuevo.length; i++){
                 if(menor > arrayNuevo[i]){
@@ -36,6 +41,7 @@ export default function contarMayorMenor(array, btnMayor, btnMenor, resultado){
                 }
             }
 
+            //Muestra el resultado en el span
             $resultado.textContent = ` El menor es: ${menor}`;
 
         }
