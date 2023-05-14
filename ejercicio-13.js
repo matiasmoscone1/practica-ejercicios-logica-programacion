598020210ca4e5fa69ae84f20fc4c71873831cc6
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-14 01:43:28 -0300

    Documentando ejercicio 13

--- ejercicio-13.js
+++ ejercicio-13.js
@@ -5,30 +5,33 @@
 
 export default function calcularPromedio(array, btn, promedio){
 
+    //Declarando variables de DOM
     const $btnPromedio = document.querySelector(btn);
-
     const $arrayPromedio = document.querySelector(array);
-
-    const arrayNuevo = [32,5,27,78,56,2,46,33,12,7];
-
     const $resultado = document.querySelector(promedio);
 
+    //Declarando array y mostrandolo en el documento
+    const arrayNuevo = [32,5,27,78,56,2,46,33,12,7];
     $arrayPromedio.textContent = `[${arrayNuevo}]`;
 
+    //Detectando evento click del boton de calcular promedio
     $btnPromedio.addEventListener("click", () => {
 
+        //Declarando variables para calcular el promedio
         let prom = 0
         let acumulador = 0;
 
-
+        //Recorriendo el array con un ciclo y sumando todos los valores para calcular promedio
         for(let i = 0; i < arrayNuevo.length; i++){
             
             acumulador += arrayNuevo[i];
 
-            prom = acumulador / arrayNuevo.length;
-
         }
 
+        //Calculando promedio
+        prom = acumulador / arrayNuevo.length;
+
+        //Mostrando resultado en el documento
         $resultado.textContent = `El promedio del array es: ${prom}`;
 
     });
