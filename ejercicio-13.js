f5e1970741f182444edaf1f9aa590b94cc1f1d6f
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-14 01:10:55 -0300

    Ejercicio 13 agregando cosas

--- ejercicio-13.js
+++ ejercicio-13.js
@@ -7,11 +7,20 @@ export default function calcularPromedio(array, btn, promedio){
 
     const $btnPromedio = document.querySelector(btn);
 
+    const $arrayPromedio = document.querySelector(array);
+
+    const arrayNuevo = [32,5,27,78,56,2,46,33,12,8];
+
+    const $resultado = document.querySelector(promedio);
+
+    $arrayPromedio.textContent = `[${arrayNuevo}]`;
 
     $btnPromedio.addEventListener("click", () => {
 
+        let prom = 0;
 
-        
+
+        $resultado.textContent = `El promedio del array es: ${prom}`;
 
     });
 
