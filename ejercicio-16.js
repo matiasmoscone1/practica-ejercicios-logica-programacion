27e7cb2bd2f6f48fea67f121aefe279112e8ee91
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-21 19:22:01 -0300

    Documentando ej 16

--- ejercicio-16.js
+++ ejercicio-16.js
@@ -1,29 +1,29 @@
 
-
-
 //16. Escribir un programa que transforme las temperaturas en Celsius y Farenheit
-
 
 export default function cambiarTemperaturas(celsius,btnCelsius,farenheit,btnFarenheit,resultado){
 
-
+    //Creando variable DOM para el resultado
     const $resultado = document.querySelector(resultado);
 
+    //Captando el evento click del DOM
     document.addEventListener("click", (e) => {
+        //Creando variables que referencian los input del DOM para sacar el valor
         const $gradosCelsius = document.querySelector(celsius).value;
         const $gradosFarenheit = document.querySelector(farenheit).value;
-
+        
+        //Captando click del boton de celsius a farenheit
         if(e.target.matches(btnCelsius)){
             let res = $gradosCelsius * 9 / 5 + 32;
-
+            //Poniendo resultado en el DOM
             $resultado.textContent = `${$gradosCelsius} C° = ${res} F°`;
         }
+        //Captando click del boton de farenheit a celsius
         if(e.target.matches(btnFarenheit)){
             let res = ($gradosFarenheit - 32) * 5/9;
-            
+            //Poniendo resultado en el DOM
             $resultado.textContent = `${$gradosFarenheit} F° = ${parseFloat(res.toFixed(1))} C°`;
         }
     })
-
 
 }
