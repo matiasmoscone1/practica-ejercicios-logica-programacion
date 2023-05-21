17395ade01297259d3abda4ee4740aec49fd4108
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-21 19:17:35 -0300

    Terminando ejercicio 16

--- ejercicio-16.js
+++ ejercicio-16.js
@@ -6,20 +6,22 @@
 
 export default function cambiarTemperaturas(celsius,btnCelsius,farenheit,btnFarenheit,resultado){
 
-    const $gradosFarenheit = document.querySelector(farenheit).value;
 
     const $resultado = document.querySelector(resultado);
 
     document.addEventListener("click", (e) => {
         const $gradosCelsius = document.querySelector(celsius).value;
-
-
+        const $gradosFarenheit = document.querySelector(farenheit).value;
 
         if(e.target.matches(btnCelsius)){
-
             let res = $gradosCelsius * 9 / 5 + 32;
 
             $resultado.textContent = `${$gradosCelsius} C° = ${res} F°`;
+        }
+        if(e.target.matches(btnFarenheit)){
+            let res = ($gradosFarenheit - 32) * 5/9;
+            
+            $resultado.textContent = `${$gradosFarenheit} F° = ${res} C°`;
         }
     })
 
