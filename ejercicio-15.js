980cf6900b824f28335c64f3a41d34229f2ab749
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-24 20:46:25 -0300

    Documentando ejercicio 15

--- ejercicio-15.js
+++ ejercicio-15.js
@@ -6,17 +6,19 @@
 
 export default function determinarDias(inputA, inputB, btn, resultado){
 
-
+    //Referenciando variables del DOM 
     const $btnDias = document.querySelector(btn);
 
     const $resultado = document.querySelector(resultado);
-
+    //Captando evento click del boton para verificar dias 
     $btnDias.addEventListener("click", () => {
+        //Sacando el valor de los inputs del DOM que puso las fechas el usuario
         const $valorA = new Date(document.querySelector(inputA).value);
         const $valorB = new Date(document.querySelector(inputB).value);
-    
+        
+        //Calculando la diferencia en milisegundos
         const dias =  $valorB.getTime() - $valorA.getTime();
-
+        //Explayando el resultado en formato dias.
         $resultado.textContent = `La diferencia son: ${dias / (1000 * 60 * 60 * 24)} dias`;
 
         //console.log($valorA, $valorB);
