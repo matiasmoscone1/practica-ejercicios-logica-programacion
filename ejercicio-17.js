708952f5cc1a939541bb05fc14d8492b7b4380b7
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-25 18:18:34 -0300

    Terminando ej 17

--- ejercicio-17.js
+++ ejercicio-17.js
@@ -5,8 +5,19 @@ export default function factorial(input, btn, resultado){
 
     const $btnFactorial = document.querySelector(btn);
 
+    const $resultadoFactorial = document.querySelector(resultado);
+
     $btnFactorial.addEventListener("click", () => {
-        
+        const $inputFactorial = document.querySelector(input).value;
+        let res = 1;
+
+        //console.log($inputFactorial);
+        for(let i = 1; i <= $inputFactorial; i++){
+            res *= i;
+            //console.log(i);
+        }
+
+        $resultadoFactorial.textContent = `El resultado es: ${res}`
 
     });
 
