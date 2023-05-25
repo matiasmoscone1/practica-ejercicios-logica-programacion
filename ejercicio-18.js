3ac0cf3e042c7a3d790d828573437b98ecd5f791
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-25 18:42:56 -0300

    Documentando el ej 18

--- ejercicio-18.js
+++ ejercicio-18.js
@@ -4,17 +4,23 @@
 
 export default function ordenarPalabras(oracion, btn, resultado){
 
-
+    //Inicializando variables del DOM
     const $btnOrdenar = document.querySelector(btn);
     const $resultadoOrdenar = document.querySelector(resultado);
 
+    //Captando evento click del boton ordenar
     $btnOrdenar.addEventListener("click", () => {
+        //Tomando el valor del input que escribe la oracion el usuario
         const $oracion = document.querySelector(oracion).value;
         //console.log($oracion);
 
+        //Codigo que ordena alfabeticamente la oracion
+        //Primero convierte en array la oracion y separa por "espacios", luego ordena el array 
+        //y para terminar junta de nuevo el array en una oracion separando con espacios las palabras
         let arrayOracion = $oracion.split(" ").sort().join(" ");
-        console.log(arrayOracion);
+        //console.log(arrayOracion);
 
+        //Mostrando resultado en el DOM
         $resultadoOrdenar.textContent = `La oracion ordenada es: "${arrayOracion}"`;
     })
 
