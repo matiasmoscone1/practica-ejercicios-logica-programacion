dcf91a8630a9a0d2cecadff116e05f18bf59acd7
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-25 18:38:08 -0300

    Trabajando en ej 18

--- ejercicio-18.js
+++ ejercicio-18.js
@@ -2,10 +2,25 @@
 
 //18. Ordenar palabras: Escribe una función que tome una cadena de palabras separadas por espacios y las devuelva en orden alfabético.
 
-export default function ordenarPalabras(){
+export default function ordenarPalabras(oracion, btn, resultado){
 
 
-    
+    const $btnOrdenar = document.querySelector(btn);
+    const $resultadoOrdenar = document.querySelector(resultado);
+
+    $btnOrdenar.addEventListener("click", () => {
+        const $oracion = document.querySelector(oracion).value;
+        //console.log($oracion);
+
+        let arrayOracion = $oracion.split(" ").sort();
+        console.log(arrayOracion);
+
+        
+
+    })
+
+
+
 }
 
 
