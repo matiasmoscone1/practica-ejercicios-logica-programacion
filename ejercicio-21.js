ec323ca12c269b3c4b70b73e2b5894f0b329fb21
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-28 18:41:23 -0300

    Trabajando en ej 21

--- ejercicio-21.js
+++ ejercicio-21.js
@@ -5,10 +5,27 @@
 // en minúsculas como en mayúsculas. Por ejemplo, para la cadena "Hola, cómo estás?", 
 //la función debería devolver 7.z
 
-export default function contarVocales(){
+export default function contarVocales(oracion, btn, resultado){
 
 
-    
+    const $btnVocales = document.querySelector(btn);
+    const $resultadoVocales = document.querySelector(resultado);
+
+    $btnVocales.addEventListener("click", () => {
+        let contador = 0;
+        const $oracion = document.querySelector(oracion).value;
+        
+        console.log($oracion);
+
+        for(let i = 0; i < $oracion.length; i++){
+            if($oracion[i] === "a" || $oracion[i] === "e" || $oracion[i] === "i" || $oracion[i] === "u" || $oracion[i] === "o"){
+                contador += 1;
+            }
+        }
+
+
+        $resultadoVocales.textContent = `Las vocales repetidas son: ${contador}`;
+    });
 
 
 
