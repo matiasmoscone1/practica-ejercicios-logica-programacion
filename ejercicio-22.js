0471c274b3f22d35a4626a4df58ffac88fb4251c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-31 12:12:17 -0300

    Terminando ej 22

--- ejercicio-22.js
+++ ejercicio-22.js
@@ -15,10 +15,24 @@ export default function palabrasAnagramas(palabra1, palabra2, btn, resultado){
 
     $btnAnagramas.addEventListener("click", () => {
 
-        const $palabra1Anagramas = document.querySelector(palabra1).value;
-        const $palabra2Anagramas = document.querySelector(palabra2).value;
+        let $palabra1Anagramas = document.querySelector(palabra1).value;
+        let $palabra2Anagramas = document.querySelector(palabra2).value;
 
-        
+        $palabra1Anagramas = $palabra1Anagramas.toLowerCase().trim();
+        $palabra2Anagramas = $palabra2Anagramas.toLowerCase().trim();
+
+
+        const caracter1 = $palabra1Anagramas.split("");
+        const caracter2 = $palabra2Anagramas.split("");
+
+        const ordenarPalabra1 = caracter1.sort().join("");
+        const ordenarPalabra2 = caracter2.sort().join("");
+
+        if(ordenarPalabra1 === ordenarPalabra2){
+            $resultadoAnagramas.textContent = `Las palabras ${$palabra1Anagramas} y ${$palabra2Anagramas} son anagarmas`; 
+        }else{
+            $resultadoAnagramas.textContent = `Las palabras ${$palabra1Anagramas} y ${$palabra2Anagramas} NO son anagramas`;     
+        }
 
     });
 
