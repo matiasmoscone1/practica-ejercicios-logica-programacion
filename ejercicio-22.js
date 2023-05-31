481a9c2690ce841300c4bad4d1191c28bfa26c4c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-05-31 11:25:14 -0300

    Agregando ej 22

---
+++ ejercicio-22.js
@@ -0,0 +1,17 @@
+
+
+
+//22. Comprobar anagramas: Escribe una función que tome dos cadenas de texto y 
+//determine si son anagramas. Dos cadenas son anagramas si contienen exactamente 
+//las mismas letras, sin importar el orden. Por ejemplo, las cadenas "roma" y "amor" 
+//son anagramas.
+
+
+export default function palabrasAnagramas(palabras, btn, resultado){
+
+    const $btnAnagramas = document.querySelector(btn);
+
+
+
+
+}
