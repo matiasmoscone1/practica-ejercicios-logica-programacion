02caf436b3d7973dca10a909c5b68f780f77d913
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-01 14:37:30 -0300

    Documentando ej 23

--- ejercicio-23.js
+++ ejercicio-23.js
@@ -5,15 +5,19 @@
 
 
 export default function invertirCadena(palabra, btn, resultado){
-
+    //Creando variables del DOM
     const $btnInvertir = document.querySelector(btn);
     const $resultadoInvertir = document.querySelector(resultado);
 
+    //Captando evento click del boton Invertir
     $btnInvertir.addEventListener("click", () => {
+        //Captando el valor introducido por teclado del input 
         const $palabraInvertir = document.querySelector(palabra).value;
-        
+        //Convierte una palabra en un array, lo da vuelta y lo 
+        //junta de nuevo como si fuera una palabra
         const nuevaPalabra = $palabraInvertir.split("").reverse().join("");
 
+        //Mostrando resultado en el DOM
         $resultadoInvertir.textContent = `La palabra invertida es: ${nuevaPalabra}`;
 
     });
