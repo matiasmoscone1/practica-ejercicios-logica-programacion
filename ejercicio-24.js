1477fb57061c2fbe487ff27447b81512ba72f4e4
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-03 19:39:07 -0300

    Terminando ejercicio 24

--- ejercicio-24.js
+++ ejercicio-24.js
@@ -18,7 +18,15 @@ export default function palabraLarga(oracion, btn, resultado){
 
         console.log(arrayPalabraLarga);
 
+        let palabraLarga = "";
 
+        for(let i = 0; i < arrayPalabraLarga.length; i++){
+            if(arrayPalabraLarga[i].length > arrayPalabraLarga.length){
+                palabraLarga = arrayPalabraLarga[i];
+            }
+        }
+        
+        console.log(palabraLarga);
     });
 
 
