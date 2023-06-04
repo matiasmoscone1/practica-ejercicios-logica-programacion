8bc245f296f303c2b38084d753256a8df290fde4
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-04 19:28:01 -0300

    Documentando ejercicio 24

--- ejercicio-24.js
+++ ejercicio-24.js
@@ -5,27 +5,34 @@
 
 export default function palabraLarga(oracion, btn, resultado){
 
+    //Creando variables del DOM
     const $btnPlabraLarga = document.querySelector(btn);
 
     const $resultadoPalabraLarga = document.querySelector(resultado);
 
+    //Captando evento click del dom
     $btnPlabraLarga.addEventListener("click", () => {
 
+        //Captando la oracion completa escrita por el usuario
         const $oracionPalabraLarga = document.querySelector(oracion).value;
-
         
+        //Convirtiendo esa oracion en un array de palabras separadas por espacio        
         const arrayPalabraLarga = $oracionPalabraLarga.split(" ");
 
         //console.log(arrayPalabraLarga);
 
+        //inicializando lo que determinara el resultado
         let palabraLarga = "";
 
+        //Ciclo for que itera tantas veces como palabras tenga el array creado anteriormente
         for(let i = 0; i < arrayPalabraLarga.length; i++){
+            //Condicional que determina la palabra de mayor longitud y la guarda en la variable creada antes
             if(arrayPalabraLarga[i].length > arrayPalabraLarga.length){
                 palabraLarga = arrayPalabraLarga[i];
             }
         }
         
+        //Mostrando resultado en el DOM
         $resultadoPalabraLarga.textContent = `La palabra mas larga es: ${palabraLarga}`;
 
         //console.log(palabraLarga);
