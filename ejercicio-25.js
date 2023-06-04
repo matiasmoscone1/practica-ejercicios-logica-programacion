8b4cf95da84e45995f8f944d87000a4d77b8c7fc
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-04 19:42:49 -0300

    Trabajando en ejercicio 25

--- ejercicio-25.js
+++ ejercicio-25.js
@@ -3,7 +3,30 @@
 //25. Conversión de binario a decimal: Escribe una función que convierta un número binario a decimal.
 
 
-export default function convertirNumeros(){
-    
+export default function convertirNumeros(numero, btn, resultado){
+
+    const $btnConversor = document.querySelector(btn);
+
+    const $resultadoConversor = document.querySelector(resultado);
+
+
+    $btnConversor.addEventListener("click", () => {
+        
+        const $numeroConvertir = document.querySelector(numero).value;
+        let numDecimal = 0;
+
+        let numeroNuevo = $numeroConvertir.split("").reverse().join("");
+
+        for(let i = 0; i < numeroNuevo.length; i++){
+            numDecimal = numDecimal + numeroNuevo[i] * 2 ** numeroNuevo.length - 1;
+        }
+
+        console.log(numDecimal);
+
+        $resultadoConversor.textContent = `El decimal es: ${numDecimal}`;
+        //console.log(numeroNuevo);
+
+    });
+
 }
 
