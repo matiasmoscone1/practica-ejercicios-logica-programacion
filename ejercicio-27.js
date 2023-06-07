629379a39d204222da4867935fb56503597dc433
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-07 18:02:50 -0300

    Documentando ej 27

--- ejercicio-27.js
+++ ejercicio-27.js
@@ -6,25 +6,31 @@
 
 export default function generarPatron(triangulo, cuadrado, resultado){
 
+    //Asignando variables para manipular el DOM
     const $btnTriangulo = document.querySelector(triangulo);
-
     const $btnCuadrado = document.querySelector(cuadrado);
-
     const $resultadoForma = document.querySelector(resultado);
 
+    //Captando evento click del boton Triangulo
     $btnTriangulo.addEventListener("click", () => {
-
+        //Asignando las variables para generar el patron, numFilas significa 
+        //cuantas veces va a iterar el ciclo for
         let numFilas = 5; 
         let patron = "";
+        
+        //Ciclo for que itera por cada linea itera otro for que le va sumando asteriscos a la linea
         for (let i = 1; i <= numFilas; i++) {
             let linea = '';
             for (let j = 1; j <= i; j++) {
               linea += '*';
             }
-            console.log(linea);
+            //console.log(linea);
+            //Se le asigna valor al patron poniendole todo el contenido que tiene "linea"
             patron += linea + "\n";
         }
         
+        //Se agrega el patron a la div de $resultadoForma y por cada salto de linea \n 
+        //se le reemplaza por un <br>  
         $resultadoForma.innerHTML = patron.replace(/\n/g, "<br>");
         
     })
