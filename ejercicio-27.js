33b83010ba8bc596459823c7f5623b403dcdbb01
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-07 17:37:23 -0300

    Resolviendo ej 27

--- ejercicio-27.js
+++ ejercicio-27.js
@@ -10,7 +10,22 @@ export default function generarPatron(triangulo, cuadrado, resultado){
 
     const $btnCuadrado = document.querySelector(cuadrado);
 
+    const $resultadoForma = document.querySelector(resultado);
 
+    $btnTriangulo.addEventListener("click", () => {
 
+        let numFilas = 5; 
+
+        for (let i = 1; i <= numFilas; i++) {
+            let linea = '';
+            for (let j = 1; j <= i; j++) {
+              linea += '*';
+            }
+            console.log(linea);
+          }
+        
+        
+        
+    })
     
 }
