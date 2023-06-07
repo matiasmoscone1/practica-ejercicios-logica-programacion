c1c62dc3fa9d6735e98b21ee1a630df6481f8897
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-07 18:19:19 -0300

    Agregando ejercicio 28

---
+++ ejercicio-28.js
@@ -0,0 +1,13 @@
+
+
+
+//28. Patrón de números ascendentes: 
+//Escribe un programa que genere un patrón de números ascendentes en forma de triángulo.
+
+
+export default function patronNumAscendentes(){
+
+
+
+    
+}
--- index.js
+++ index.js
@@ -25,6 +25,7 @@ import palabraLarga from "./ejercicio-24.js";
 import convertirNumeros from "./ejercicio-25.js";
 import numeroPerfecto from "./ejercicio-26.js";
 import generarPatron from "./ejercicio-27.js";
+import patronNumAscendentes from "./ejercicio-28.js";
 
 document.addEventListener("DOMContentLoaded", () => {
 
@@ -55,5 +56,6 @@ document.addEventListener("DOMContentLoaded", () => {
     convertirNumeros(".input-binario-decimal", ".btn-ej-25", ".resultado-ej-25");
     numeroPerfecto(".input-numero-perfecto", ".btn-ej-26", ".resultado-ej-26");
     generarPatron(".generar-triangulo", ".generar-cuadrado", ".limpiar-patron", ".resultado-ej-27");
+    patronNumAscendentes(".generar-patron-num-ascendente", ".limpiar-patron-ej-28", ".resultado-ej-28");
 
 });
