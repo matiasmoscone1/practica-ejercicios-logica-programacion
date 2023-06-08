b3f5cdb1b0982aa05f7a872a5606fa188bf7c423
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-08 01:22:59 -0300

    Documentando ej 28

--- ejercicio-28.js
+++ ejercicio-28.js
@@ -7,32 +7,40 @@
 
 export default function patronNumAscendentes(btnGenerar, btnLimpiar, resultado){
 
+    //Creando variables referenciales al DOM
     const $btnGenerar = document.querySelector(btnGenerar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultadoPatron = document.querySelector(resultado);
 
-
+    //Captando evento click del boton Generar 
     $btnGenerar.addEventListener("click", () => {
 
+        //Creando variables para iterar en el ciclo y generar el patron
         let numFila = 5;
         let patron = "";
 
+        //Ciclo que genera el patron linea por linea
         for(let i = 1; i <= numFila; i++){
+            //inicializo la linea con un string vacio
             let linea = '';
+            //ciclo que por cada linea suma el valor de i
             for(let j = 1; j <= i; j++){
                 linea += i;
             }
+            //va sumando las lineas listas al patron y las separa con un salto de linea \n
             patron += linea + '\n';
         }
         
         //console.log(patron);
 
+        //Muestra resultado por DOM reemplazando el salto de linea \n por una etiqueta br
         $resultadoPatron.innerHTML = patron.replace(/\n/g, "<br>");
 
     });
 
+    //Captando evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
-
+        //Restablece la div de $resultadoPatron en un string vacio
         $resultadoPatron.innerHTML = "";
 
     });
