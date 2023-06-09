a6e86bd193277f9d9d70a93996f1a3248141f041
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-09 17:51:05 -0300

    Agregando ej 29

---
+++ ejercicio-29.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+//29. Patrón de números en espiral: Escribe un programa que genere un patrón de 
+//números en forma de espiral cuadrada. 
+ 
+export default function generarPatronEspiral(){
+
+
+
+
+    
+}
+
+
+
+
+
+
+
+
+
+
