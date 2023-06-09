83a0012c24d06dbf1055fe68a4e972b4f0202f30
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-09 18:03:54 -0300

    Comentando como se hace el ej 29

--- ejercicio-29.js
+++ ejercicio-29.js
@@ -12,8 +12,23 @@ export default function generarPatronEspiral(btn, btnLimpiar, resultado){
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultadoPatronEspiral = document.querySelector(resultado);
 
+    $btnGenerar.addEventListner("click", () => {
+        const n = 5;
 
-    
+        //primero debo crear una matriz con dos ciclos que recorra mientras que sea menor o
+        //igual a n (n)
+
+
+        //tengo que declarar las variables de valor y limiteSuperior, limiteInferior, 
+        //limiteIzquierdo y limiteDerecho
+
+
+        //creo un ciclo while de valor = 1 mientras que sea menor o igual que n * n
+
+        
+
+    });
+
 
 
 }
