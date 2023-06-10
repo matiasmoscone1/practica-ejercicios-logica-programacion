340efe45a81360937c134a0b17751e48f77d036c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-10 19:46:57 -0300

    Resolver ej 30 pendiente

--- ejercicio-30.js
+++ ejercicio-30.js
@@ -3,10 +3,20 @@
 //30. Escribe un programa que genere los primeros n números primos. 
 //Un número primo es aquel que solo es divisible por 1 y por sí mismo.
 
+function esPrimo(numero){
+
+    if(numero < 2){
+        return false;
+    }
+    for(let i = 2; i < numero; i++){
+        if(numero % i === 0){
+            return false;
+        }
+    }
+}
 
 
 export default function generarNumerosPrimos(inputNumeros, btnGenerar, btnLimpiar, resultado){
-
 
     const $btnGenerar = document.querySelector(btnGenerar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
@@ -15,13 +25,16 @@ export default function generarNumerosPrimos(inputNumeros, btnGenerar, btnLimpia
     $btnGenerar.addEventListener("click", () => {
         const $inputNumeros = document.querySelector(inputNumeros).value;
         let arrayNumeros = [];
-        for(let i = 0; i <= $inputNumeros; i++){
-            if((i / 1 === 0) && (i % i === 0)){
-                arrayNumeros.push(i);
+        let contador = 2;
+        /*while(arrayNumeros.length < $inputNumeros){
+            if(esPrimo(contador)){
+                arrayNumeros.push(contador);
             }
-        }
+            contador++; 
+        }*/
+
         
-        console.log(arrayNumeros);
+        //console.log(arrayNumeros);
 
     });
 
