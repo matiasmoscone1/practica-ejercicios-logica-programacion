94a6b4d064337bebe567a67568f41d3b1b8023e2
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-14 16:37:33 -0300

    Resolviendo ej 32

--- ejercicio-32.js
+++ ejercicio-32.js
@@ -10,10 +10,24 @@
 
 export default function generarContrasenia(btnGenerador, btnLimpiar, resultado){
 
-    
+    const $btnGenerar = document.querySelector(btnGenerador);
+    const $btnLimpair = document.querySelector(btnLimpiar);
+    const $resultadoGenerarContrasenia = document.querySelector(resultado);
 
+    $btnGenerar.addEventListener("click", () => {
+        
+        let contrasenia = 0;
+        /*let objeto = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,
+        m,n,o,p,q,s,t,r,x,w,y,z}
+            */
 
+        let obj = {A: "A", B: "B", C: "C", a: "a", b: "b", c:"c"};
+        contrasenia = Math.random(1, 1000) * 1000 + Math.random(obj.value);
 
+        //console.log(Math.floor(contrasenia));
+            console.log(contrasenia);
+
+    });
 
 
 }
