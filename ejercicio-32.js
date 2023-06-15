c2f06ebd4e5d2fc41e1cf901cddef8e395525aed
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-15 18:18:14 -0300

    array terminado de caracteres alfanumericos y especiales

--- ejercicio-32.js
+++ ejercicio-32.js
@@ -17,20 +17,16 @@ export default function generarContrasenia(btnGenerador, btnLimpiar, resultado){
     $btnGenerar.addEventListener("click", () => {
         
         let contrasenia = "";
-        /*let objeto = {A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,W,X,Y,Z,a,b,c,d,e,f,g,h,i,j,k,l,
-        m,n,o,p,q,s,t,r,x,w,y,z}
-            */
-
-        let obj = ["0","1","2","3","4","5","6","7","8","9","A","B","C","a","b","c"];
-        //contrasenia = Math.random(1, 1000) * 1000 + Math.random(obj.value);
-
-        //console.log(Math.floor(contrasenia));
         
 
+        let obj = ["0","1","2","3","4","5","6","7","8","9","-","_","!","@","#","$","%","&","/",
+        "(",")","=","?","¡","¿","|","°","{","}","^","]","[",".","<",">","~","A","B","C","D","E",
+        "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z","a","b","c",
+        "d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","r","x","w","y","z"];
+        
         for(let i = 0; i < 11; i++){
             let generadorContrasenia = Math.random(0, obj.length) * obj.length;
             contrasenia += obj[Math.floor(generadorContrasenia)];
-    
         }
 
         console.log(contrasenia);
