62756b7dd00d79852ff003da73e9fb0cb19edb5d
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-16 15:22:45 -0300

    Documentando ej 33

--- ejercicio-33.js
+++ ejercicio-33.js
@@ -5,13 +5,17 @@
 
 export default function contadorPulsaciones(btnMostrar, btnLimpiar, resultado){
 
+    //Creando variables referenciales al DOM
     const $btnMostrar = document.querySelector(btnMostrar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultadoPulsaciones = document.querySelector(resultado);
 
+    //inicializando contador de pulsadas de enter
     let contador = 0;
 
+    //captando evento de una tecla
     document.addEventListener("keyup", (e) => {
+        //verificando si la tecla presionada es enter, sumamos al contador 1
         if(e.key === "Enter"){
             contador += 1;
         }
@@ -19,11 +23,15 @@ export default function contadorPulsaciones(btnMostrar, btnLimpiar, resultado){
         console.log(contador);
     });
 
+    //captando evento click del boton mostrar para mostrar resultado en DOM
     $btnMostrar.addEventListener("click", () => {
+        //mostrando resultado en DOM
         $resultadoPulsaciones.innerHTML = `La cantidad de veces pulsadas enter son: ${contador}`;
     });
 
+    //captando evento click de boton limpiar para limpiar el DOM
     $btnLimpiar.addEventListener("click", () => {
+        //limpiando DOM
         $resultadoPulsaciones.innerHTML = "";
     });
 
