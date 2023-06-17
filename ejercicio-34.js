c5350f182bf5cf69042352dab180171a11d24ea9
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-17 15:43:02 -0300

    Resolviendo ej 34

---
+++ ejercicio-34.js
@@ -0,0 +1,25 @@
+
+
+
+//34. Contador de vocales: Desarrolla un programa que registre y muestre en la página HTML 
+//la cantidad de veces que se han pulsado las vocales (a, e, i, o, u) en cualquier orden.
+
+
+
+export default function contadorVocalesTeclado(btnMostrar, btnLimpiar, resultado){
+
+    const $btnMostrar = document.querySelector(btnMostrar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+
+
+
+
+
+    
+}
+
+
+
+
+
+
