99be046666119fe9c8b8c14cbd8c880ab1c0ef46
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-19 16:01:51 -0300

    Resolviendo ej 35

--- ejercicio-35.js
+++ ejercicio-35.js
@@ -10,6 +10,20 @@
 
 export default function adivinaLaTecla(generador, btnMostrar, btnLimpiar, resultado){
 
+    const $btnGenerador = document.querySelector(generador);
+    const $btnMostrar = document.querySelector(btnMostrar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultadoTecla = document.querySelector(resultado);
+
+    let letraGenerada = "";
+
+    $btnGenerador.addEventListener("click", () => {
+        const arrayLetra = ["A","B","C","D","E",
+        "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z"];
+        let generadorLetra = Math.random(0, arrayLetra.length) * arrayLetra.length;
+        letraGenerada = arrayLetra[Math.floor(generadorLetra)];
+        console.log(letraGenerada);
+    });
     
 
 
