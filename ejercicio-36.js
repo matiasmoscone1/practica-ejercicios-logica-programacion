29b7d5b104f3dbb950524111bec070cda91c3c88
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-19 17:26:48 -0300

    Resolviendo ej 36

--- ejercicio-36.js
+++ ejercicio-36.js
@@ -5,14 +5,29 @@
 //Al intentar presionar las teclas bloqueadas, no se debe reflejar ninguna acción.
 
 
-export default function bloqueadorDeTeclas(btnJugar, btnStop, input){
+export default function bloqueadorDeTeclas(btnJugar, btnStop, letras, input){
 
+    const $letrasBloqueadas = document.querySelector(letras);
 
     document.addEventListener("click", (e) => {
         const $inputTexto = document.querySelector(input);
 
         if(e.target.matches(btnJugar)){
-
+            $letrasBloqueadas.innerHTML = `Las letras bloqueadas son: A, H, K, Y, C`;
+             document.addEventListener("keyup", (e) => {
+                switch(e.key){
+                    case e.key === "A": "";
+                    break;
+                    case e.key === "H": "";
+                    break;
+                    case e.key === "K": "";
+                    break;
+                    case e.key === "Y": "";
+                    break;
+                    case e.key === "C": "";
+                    break;
+                }
+             });
         }
         if(e.target.matches(btnStop)){
 
