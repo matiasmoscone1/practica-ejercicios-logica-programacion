5911438d5dec2c6a4eeba62ee17ac2841d766000
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-21 16:21:46 -0300

    Resolviendo ej 38

--- ejercicio-38.js
+++ ejercicio-38.js
@@ -9,9 +9,29 @@ export default function contadorTeclado(btnJugar, btnStop, resultado){
     const $btnJugar = document.querySelector(btnJugar);
     const $btnStop = document.querySelector(btnStop);
     const $resultado = document.querySelector(resultado);
+    const bloqActivo = false;
+    let contador = 0;
 
+    $btnJugar.addEventListener("click", () => {
+        bloqActivo = true;
 
-    
+        if(bloqActivo){
+            document.addEventListener("keydown", (e) => {
+                if(e.key === "ArrowUp"){
+                    contador += 1;
+                }
+                if(e.key === "ArrowDown"){
+                    contador -= 1;
+                }
+            });
+
+        }
+    });
+
+    $btnStop.addEventListener("click", () => {
+        
+    });
+
 
 }
 
