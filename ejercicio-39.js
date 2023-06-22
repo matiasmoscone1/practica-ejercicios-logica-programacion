3805fd6a2651e5a01134f96b54d4acb0f7b79cca
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-22 15:54:05 -0300

    Documentando ej 39

--- ejercicio-39.js
+++ ejercicio-39.js
@@ -6,14 +6,18 @@
 
 export default function formTeclado(formulario, input, resultado){
 
+    //Inicializando variables referenciales al DOM
     const $input = document.querySelector(input);
     const $formulario = document.querySelector(formulario);
     const $resultado = document.querySelector(resultado);
 
+    //Frenando el evento del submit para que no se "envie" el formulario
     $formulario.addEventListener("submit", (e) => {
         e.preventDefault();
     });
 
+    //Captando evento de tecla, si la tecla es igual a la tecla "Enter", muestra en el DOM
+    //el resultado
     document.addEventListener("keydown", (e) => {
         if(e.key === "Enter"){
             $resultado.textContent = `Hola ${$input.value} Komo e posible ete suceso`;
