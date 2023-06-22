c7a8c8569d3cf35e92b01756652afce89a8279a0
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-22 15:40:24 -0300

    Agregando ej 39

---
+++ ejercicio-39.js
@@ -0,0 +1,22 @@
+
+
+
+//39. Crea un formulario que permita al usuario introducir su nombre y, al presionar la 
+//tecla "Enter", muestre un mensaje de bienvenida con el nombre ingresado.
+
+export default function formTeclado(formulario, input){
+
+    const $input = document.querySelector(input);
+
+    document.addEventListener("keydown", (e) => {
+        
+
+    });
+
+
+}
+
+
+
+
+
