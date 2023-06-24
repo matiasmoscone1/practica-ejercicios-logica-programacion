b8a348c8d73df0891d03c28d959550331e7a608b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-23 21:21:59 -0300

    Resolviendo ej 40

--- ejercicio-40.js
+++ ejercicio-40.js
@@ -3,7 +3,28 @@
 //y el jugador debe repetirlo presionando las teclas correspondientes.
 
 
-export default function simonSays(){
+export default function simonSays(btnJugar, btnStop, letraSimon){
+
+    const $btnJugar = document.querySelector(btnJugar);
+    const $btnStop = document.querySelector(btnStop);
+    let juegoActivo = false;
+    const $letraSimon = document.querySelector(letraSimon);
+
+
+
+    $btnJugar.addEventListener("click", () => {
+        juegoActivo = true;
+        const contenedorLetras = ["A","B","C","D","E","F","G","H","I","J","K","M","N","L","O",
+        "P","Q","R","S","T","X","Y","W","Z"];
+
+        if(juegoActivo){
+            
+
+        }
+
+    });
+    
+    
 
 
 
