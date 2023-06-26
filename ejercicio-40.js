9403f7ace15cb8645c77c536e735d7b76601dde2
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-26 16:04:25 -0300

    Resolviendo ej 40

--- ejercicio-40.js
+++ ejercicio-40.js
@@ -3,17 +3,19 @@
 //y el jugador debe repetirlo presionando las teclas correspondientes.
 
 
-export default function simonSays(btnJugar, btnStop, letraSimon){
+export default function simonSays(btnJugar, btnStop, contadorSimonSays, letraSimon){
 
     const $btnJugar = document.querySelector(btnJugar);
     const $btnStop = document.querySelector(btnStop);
     let juegoActivo = false;
     const $letraSimon = document.querySelector(letraSimon);
+    const $resultadoContador = document.querySelector(contadorSimonSays);
     
 
 
     $btnJugar.addEventListener("click", () => {
         juegoActivo = true;
+        let contador = 0;
         const contenedorLetras = ["A","B","C","D","E","F","G","H","I","J","K","M","N","L","O",
         "P","Q","R","S","T","X","Y","W","Z"];
 
@@ -27,13 +29,17 @@ export default function simonSays(btnJugar, btnStop, letraSimon){
             document.addEventListener("keydown", (e) => {
                 if(e.key === letraGenerada){
                     $letraSimon.textContent = `BIEN`;
+                    contador += 1;
                 }else{
                     $letraSimon.textContent = `Letra equivocada`;
+                    contador = 0;
                 }
             });
             console.log(letraGenerada);
             $letraSimon.textContent = `${letraGenerada}`;
             
+            $resultadoContador.textContent = `Puntaje: ${contador}`;
+
         }
 
     });
