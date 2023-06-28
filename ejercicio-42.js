20ee4c65bfa361422f85a038e983d231ccaab95d
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-06-28 16:32:06 -0300

    Comenzando ej 42

--- ejercicio-42.js
+++ ejercicio-42.js
@@ -7,10 +7,14 @@
 //marcarlas como completadas utilizando la tecla "Espacio".
 
 
-export default function taskList(){
+export default function taskList(btnEmpezar, btnTerminar, btnLimpiar, resultado){
+
+    const $btnEmpezar = document.querySelector(btnEmpezar);
+    const $btnTerminar = document.querySelector(btnTerminar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultadoTaskList = document.querySelector(resultado);
 
 
-    
 }
 
 
