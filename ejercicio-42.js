98a3805f6997268ff7d97d8cc7aadf29e09d201b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-03 21:29:07 -0300

    Documentando ej 42

--- ejercicio-42.js
+++ ejercicio-42.js
@@ -6,20 +6,26 @@
 
 export default function taskList(btnEmpezar, btnLimpiar){
 
+    //Declarando variables referenciales al DOM
     const $btnEmpezar = document.querySelector(btnEmpezar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
+    //Declarando variables de bandera para lista de tareas activa y un index que refleja el
+    //numero de checkbox a marcar.
     let bloqTaskList = false;
     let index = 1;
     
-
+    //Funcion que controla el evento keyup apretado cuando se le da click al boton empezar
     const keyUpHandler = (e) => {
+        //Si el evento de teclado es la barra espaciadora, crea una variable check que referencia
+        //cada checkbox del DOM. !e.repeat => sirve para q no se repita el evento mas de 1 vez.
         if(e.code === "Space" && !e.repeat){
-            //console.log("ASDASD") 
             const check = document.querySelector(`.check-${index}`);
+            //si check existe, pone el .checked de la checkbox en true
             if(check){
                 check.checked = true; 
+                //aumenta en 1 el index para que el proximo keyup vaya dirigido a la proxima
+                //checkbox
                 index++;
-                console.log(index);
             }
         }
     };
