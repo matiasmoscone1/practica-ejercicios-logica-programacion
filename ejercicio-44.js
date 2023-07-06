bdb8a20c744eacfc78c2851b9d25b52011e8e200
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-06 18:03:57 -0300

    Terminando ejercicio 44

--- ejercicio-44.js
+++ ejercicio-44.js
@@ -9,10 +9,22 @@ export default function filtrarPalabrasCincoLetras(btnFiltrar, btnLimpiar, textA
 
     const $btnFiltrar = document.querySelector(btnFiltrar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
-    const $textArea = document.querySelector(textArea);
     const $resultado = document.querySelector(resultado);
 
 
+    
+    $btnFiltrar.addEventListener("click", () => {
+        const $textArea = document.querySelector(textArea).value;
+        let nuevoArray = $textArea.split(" ");
+        let arrayFiltrado = [];
+        for(let i = 0; i < nuevoArray.length; i++){
+            if(nuevoArray[i].length > 5){
+                arrayFiltrado.push(nuevoArray[i]);
+            }
+        }
+
+        console.log(arrayFiltrado);
+    });
 
 
 }
