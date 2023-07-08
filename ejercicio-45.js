003b3eb76de8cb9cbda9e82082f90285baf8ffbb
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-08 19:56:13 -0300

    Ej 45 terminado

--- ejercicio-45.js
+++ ejercicio-45.js
@@ -7,13 +7,13 @@
 export default function personaJovenYMayor(personas, btnMayor, btnMenor, btnLimpiar,resultado){
 
 
+    //Creando las variables referenciales al DOM (no cree las de los botones porque utilize
+    //el metodo e.target.matches)
     const $personas = document.querySelector(personas);
-    //const $btnMayor = document.querySelector(btnMayor);
-    //const $btnMenor = document.querySelector(btnMenor);
-    //const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
 
+    //Creando array de objetos de personas
     const arrayPersonas = [{
         nombre: "Matias",
         apellido: "Moscone",
@@ -41,12 +41,15 @@ export default function personaJovenYMayor(personas, btnMayor, btnMenor, btnLimp
     }
 ];
 
+    //Visualizando el array de personas en el DOM
     $personas.innerHTML = `${JSON.stringify(arrayPersonas)}`;
+    //Creando variables mayor y menor iniciandolas con la primera persona del array
     let mayor = arrayPersonas[0];
     let menor = arrayPersonas[0];
 
-    console.log(arrayPersonas);
+    //console.log(arrayPersonas);
 
+    //Ciclo que recorre el array de personas y determina el mayor y el menor en cuanto a la edad
     for(let i = 0; i < arrayPersonas.length; i++){
         if(arrayPersonas[i].edad > mayor.edad){
             mayor = arrayPersonas[i];
@@ -58,16 +61,19 @@ export default function personaJovenYMayor(personas, btnMayor, btnMenor, btnLimp
 
     //console.log(btnMayor);
 
-
+    //Captando evento click del DOM
     document.addEventListener("click", (e) => {
+        //Si el evento click matchea con el btnMayor muestra la persona mayor
         if(e.target.matches(btnMayor)){
             $resultado.innerHTML = `El mayor es: ${mayor.nombre} ${mayor.apellido} con ${mayor.edad} años`;
             //console.log(mayor);            
         }
+        //Si el evento click matchea con el btnMenor muestra la persona menor
         if(e.target.matches(btnMenor)){
             $resultado.innerHTML = `El menor es: ${menor.nombre} ${menor.apellido} con ${menor.edad} años`;               
             //console.log(menor);
         }
+        //Si el evento click matchea con el btnLimpiar limpia el resultado del DOM
         if(e.target.matches(btnLimpiar)){
             $resultado.innerHTML = "";
         }
