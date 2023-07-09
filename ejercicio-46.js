95846e8cc1a3ca35043931ed3b1c449d7befb50e
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-09 15:46:28 -0300

    Documentando ej 46

--- ejercicio-46.js
+++ ejercicio-46.js
@@ -8,34 +8,47 @@
 
 export default function convertirDecimalABinario(inputNumero, btnConvertir, btnLimpiar, resultado){
 
+    //Creando variables del DOM
     const $btnConvertir = document.querySelector(btnConvertir);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //captando evento click del boton Convertir
     $btnConvertir.addEventListener("click", () => {
+        //tomando por parametro el valor del input del usuario
         const $inputNumero = document.querySelector(inputNumero).value;
     
+        //Creando variable decimal que almacena lo q puso en el input el usuario y creando variable
+        //binario como una cadena vacia
         let decimal = $inputNumero;
         let binario = "";
 
         //console.log($inputNumero);
 
+        //bucle que se ejecuta hasta que la variable decimal sea 0
         while(decimal > 0){
+            //crea el residuo y le almacena el valor
             let residuo = decimal % 2;
+            //concatena a la cadena "binario" el residuo que va dejando la operacion
             binario += residuo;
+            //asigna a la variable decimal el resultado de dividirlo por 2
             decimal = Math.floor(decimal / 2);
         }
 
+        //junta todo y lo transforma en una cadena de numeros
         binario.split("").reverse().join("");
 
         //console.log(binario);
 
+        //Muestra el resultado por pantalla
         $resultado.innerHTML = `El binario es: ${binario}`;
 
 
     });
 
+    //Captando el evento click del boton limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //limpia el resultado de la pantalla
         $resultado.innerHTML = "";
     });
 
