1c5ae5ce9f140fd32fa76ecb1efd01a2be0c9361
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-10 15:09:14 -0300

    Ej 47 resuelto

--- ejercicio-47.js
+++ ejercicio-47.js
@@ -9,15 +9,31 @@
 
 export default function buscarPalabra(textArea, inputPalabra, btnBuscar, btnLimpiar, resultado){
 
-    const $textArea = document.querySelector(textArea).value;
-    const $inputPalabra = document.querySelector(inputPalabra);
 
     const $btnBuscar = document.querySelector(btnBuscar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
-
+    const $resultado = document.querySelector(resultado);
 
     $btnBuscar.addEventListener("click", () => {
-        
+        const $textArea = document.querySelector(textArea).value;
+        const $inputPalabra = document.querySelector(inputPalabra).value;
+        let contador = 0;
+
+        //console.log($inputPalabra);
+        let arrayOracion = $textArea.split(" ");
+        //console.log(arrayOracion);
+
+        for(let i = 0; i < arrayOracion.length; i++){
+            if($inputPalabra === arrayOracion[i]){
+                contador++; 
+            }
+        }
+        //console.log(contador);
+
+        $resultado.textContent = `La palabra "${$inputPalabra}" se repite: ${contador} veces`;
+
+
+
     });
     
 }
