9bffdc63e21e78792903f74f655ecf208b6115be
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-10 15:16:30 -0300

    Documentando ej 47

--- ejercicio-47.js
+++ ejercicio-47.js
@@ -9,31 +9,42 @@
 
 export default function buscarPalabra(textArea, inputPalabra, btnBuscar, btnLimpiar, resultado){
 
-
+    //Creando variables referenciales al DOM
     const $btnBuscar = document.querySelector(btnBuscar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //captando evento click del boton buscar
     $btnBuscar.addEventListener("click", () => {
+        //tomando el valor del textarea e input escrito por el usuario
         const $textArea = document.querySelector(textArea).value;
         const $inputPalabra = document.querySelector(inputPalabra).value;
+        //creando variable contador
         let contador = 0;
 
         //console.log($inputPalabra);
+        //Creando array para separar por "espacio en blanco" cada palabra del textarea
         let arrayOracion = $textArea.split(" ");
         //console.log(arrayOracion);
 
+        //Ciclo que itera la cantidad de veces por palabra tenga el array creado
         for(let i = 0; i < arrayOracion.length; i++){
+            //si encuentra la palabra escrita por el usuario en una iteracion, pone el contador
+            //en +1
             if($inputPalabra === arrayOracion[i]){
                 contador++; 
             }
         }
         //console.log(contador);
+        //Mostrando el resultado por el DOM
         $resultado.textContent = `La palabra "${$inputPalabra}" se repite: ${contador} veces`;
 
     });
 
+    //captando evento click del boton limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //Limpia tanto lo que tiene el textarea y el input dentro, sino que tambien el resultado
+        //mostrado anteriormente en el DOM
         document.querySelector(textArea).value = "";
         document.querySelector(inputPalabra).value = "";
         $resultado.textContent = "";
