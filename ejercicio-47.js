00a7fd45f642178823bce7399d32e7267fbb74ae
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-10 14:54:43 -0300

    Agregando ej 47

---
+++ ejercicio-47.js
@@ -0,0 +1,21 @@
+
+
+
+
+//47.Ejercicio de búsqueda de palabras: Escribe un programa que solicite al usuario ingresar 
+//una oración y una palabra por teclado. Luego, el programa debe contar cuántas veces 
+//aparece esa palabra en la oración ingresada y mostrar el resultado.
+
+
+export default function buscarPalabra(){
+
+
+
+
+    
+}
+
+
+
+
+
