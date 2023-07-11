9f4e979f4c21c1c54df043cff15bd13bfd4a9fe1
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-11 16:27:24 -0300

    Ejercicio 48 terminado

--- ejercicio-48.js
+++ ejercicio-48.js
@@ -5,10 +5,43 @@
 //ingresada es válida y mostrar un mensaje indicando si es correcta o no.
 
 
-export default function validacionFecha(){
+export default function validacionFecha(inputFecha, btnValidar, btnLimpiar, resultado){
+
+    const $btnValidar = document.querySelector(btnValidar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+
+    let formatoFecha = /^\d{2}\/\d{2}\/\d{4}$/;
 
 
-    
+    $btnValidar.addEventListener("click", () => {
+
+        const $inputFecha = document.querySelector(inputFecha).value;
+
+        console.log($inputFecha);
+
+        if(formatoFecha.test($inputFecha)){
+            let divFecha = $inputFecha.split("/");
+            let dia = parseInt(divFecha[0], 10);
+            let mes = parseInt(divFecha[1], 10);
+            let anio = parseInt(divFecha[2], 10);
+        
+            if(dia >= 1 && dia <= 31 && mes >= 1 && mes <= 12 && anio >= 1900 && anio <= 2050){
+                console.log("FECHA VALIDA");
+            }else{
+                console.log("FECHA INVALIDA");
+            }
+        }else{
+            console.log("EL FORMATO NO COINCIDE");
+        }
+
+        
+
+
+
+
+    });
+
+
 }
 
 
