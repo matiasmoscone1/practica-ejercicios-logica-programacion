62ecac038e23fb257f7eba73bfb77d064fab8b65
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-13 15:44:39 -0300

    Resolviendo ej 49

--- ejercicio-49.js
+++ ejercicio-49.js
@@ -6,21 +6,27 @@
 
 export default function convertirMetros(inputMetros, btnKilometros, btnCentimetros, btnMillas, resultado){
 
-    
+    const $resultado = document.querySelector(resultado);
+
     document.addEventListener("click", (e) => {
 
         const $inputMetros = document.querySelector(inputMetros).value;
 
-        console.log($inputMetros);
+        let calculo = 0;
+
+        //console.log($inputMetros);
 
         if(e.target.matches(btnKilometros)){
-            
+            calculo = $inputMetros / 1000;
+            console.log(calculo);    
         }
         if(e.target.matches(btnCentimetros)){
-
+            calculo = $inputMetros * 100;
+            console.log(calculo);
         }
         if(e.target.matches(btnMillas)){
-
+            calculo = $inputMetros / 1609;
+            console.log(calculo);
         }
 
     });
