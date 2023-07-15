75d5e6cd8b1bd19d5ab1f82012783882425722ae
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-15 15:29:26 -0300

    Resolviendo ej 51

--- ejercicio-51.js
+++ ejercicio-51.js
@@ -8,7 +8,27 @@
 export default function calculoEdad(inputFecha, btnCalcular, btnLimpiar, resultado){
 
 
-    
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
+
+    $btnCalcular.addEventListener("click", () => {
+        const $inputFecha = document.querySelector(inputFecha).value;
+        let arrayFecha = $inputFecha.split("-");
+        console.log(arrayFecha);
+
+        let anio = parseInt(arrayFecha[0]);
+        let mes = parseInt(arrayFecha[1]);
+        let dia = parseInt(arrayFecha[2]); 
+
+        console.log(dia, mes, anio);
+
+        const fechaHoy = new Date();
+
+        console.log(fechaHoy.getMonth() + 1);//Mes
+        console.log(fechaHoy.getDate());//Dia
+        console.log(fechaHoy.getFullYear());//Año
+    });
 
 
 
