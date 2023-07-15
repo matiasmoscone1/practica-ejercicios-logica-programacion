e8caf45573bdcc79289875124b0808ef295d0eb1
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-15 14:37:19 -0300

    Documentando ej 50

--- ejercicio-50.js
+++ ejercicio-50.js
@@ -9,34 +9,45 @@
 
 export default function adivinaElNumero(btnGenerar, btnLimpiar, formulario, inputForm, resultado){
 
+    //Creando variables referenciales al DOM
     const $btnGenerar = document.querySelector(btnGenerar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
-
     const $formulario = document.querySelector(formulario);
     const $resultado = document.querySelector(resultado);
 
+    //Creando variable para generar el numero a adivinar
     let generarNum = "";
 
+    //captando evento click del boton Generar, genera un numero aleatorio entre 0 y 9
     $btnGenerar.addEventListener("click", () => {
         generarNum = parseInt(Math.random(0, 10) * 10);
         //console.log(generarNum);
     });
 
+    //captando evento submit del formulario
     $formulario.addEventListener("submit", (e) => {
+        //previniendo evento por defecto para que no mande la request
         e.preventDefault();
+        //tomando el valor del input escrito por el usuario
         const $inputForm = document.querySelector(inputForm).value;
         //console.log($inputForm);
         //console.log(generarNum);
+        //Se lo transforma a "number" al valor escrito por usuario y se lo compara con el
+        //numero generado
         if(parseInt($inputForm) === generarNum){
-            console.log("HAS ADIVINADO EL NUMERO!");
+            //console.log("HAS ADIVINADO EL NUMERO!");
+            //Muestra resultado por DOM
             $resultado.textContent = "HAS ADIVINADO EL NUMERO!";
         }else{
-            console.log("NO ADIVINASTE EL NUMERO");
+            //console.log("NO ADIVINASTE EL NUMERO");
+            //Muestra resultado por DOM
             $resultado.textContent = "NO ADIVINASTE EL NUMERO";
         }
     });
 
+    //captando evento click del boton limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //Limpia input, DOM y consola
         document.querySelector(inputForm).value = "";
         $resultado.textContent = "";
         console.clear();
