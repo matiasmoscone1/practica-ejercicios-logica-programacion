cddd52df6e1f1bc78b7b6d56c304154315525cbb
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-16 01:53:27 -0300

    Ejercicio 52 resuelto

--- ejercicio-52.js
+++ ejercicio-52.js
@@ -8,18 +8,27 @@
 
 export default function calculadoraMatematica(btnSuma, btnResta, btnMult, btnDivi, input1, input2, resultado){
 
-
+    const $resultado = document.querySelector(resultado);
+    const $contenedor = document.querySelector(".container-ej-52");
+    let result = 0;
     
-    document.querySelector("click", (e) => {
-        const $input1 = document.querySelector(input1).value;
-        const $input2 = document.querySelector(input2).value;
+    $contenedor.addEventListener("click", (e) => {
+        const $input1 = parseInt(document.querySelector(input1).value);
+        const $input2 = parseInt(document.querySelector(input2).value);
         if(e.target.matches(btnSuma)){
-
+            result = $input1 + $input2;
         }
-
+        if(e.target.matches(btnResta)){
+            result = $input1 - $input2;
+        }
+        if(e.target.matches(btnMult)){
+            result = $input1 * $input2;    
+        }
+        if(e.target.matches(btnDivi)){
+            result = $input1 / $input2;
+        }
+        $resultado.textContent = `El resultado es: ${result}`;
     });
-
-
 
 }
 
