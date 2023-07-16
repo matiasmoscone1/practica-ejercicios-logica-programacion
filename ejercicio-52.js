813a52760dd3f6e2a4c231a1b4f885ecabb49a52
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-16 01:47:56 -0300

    Resolviendo ej 52

--- ejercicio-52.js
+++ ejercicio-52.js
@@ -6,6 +6,21 @@
 //52. Calculadora de expresiones matemáticas: Crea una calculadora que pueda evaluar y resolver 
 //expresiones matemáticas básicas, como sumas, restas, multiplicaciones y divisiones.
 
+export default function calculadoraMatematica(btnSuma, btnResta, btnMult, btnDivi, input1, input2, resultado){
 
+
+    
+    document.querySelector("click", (e) => {
+        const $input1 = document.querySelector(input1).value;
+        const $input2 = document.querySelector(input2).value;
+        if(e.target.matches(btnSuma)){
+
+        }
+
+    });
+
+
+
+}
 
 
