52d7c85796b438693b9b161d69e961d6f4bcd5db
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-18 15:48:00 -0300

    Resolviendo ej 54

--- ejercicio-54.js
+++ ejercicio-54.js
@@ -2,10 +2,26 @@
 
 //54. Generador de tarjetas de crédito: Desarrolla una función que genere números de 
 //tarjetas de crédito válidos y aleatorios, teniendo en cuenta las reglas de 
-//validación de números de tarjetas.
+//validación de números de tarjetas (16 numeros separados en 4 grupos de 4)
 
 export default function generadorTarjetasCredito(btnGenerar, btnLimpiar, resultado){
-    
+
+    const $btnGenerar = document.querySelector(btnGenerar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+
+    const arrNumeros =  ["0","1","2","3","4","5","6","7","8","9"];
+
+    $btnGenerar.addEventListener("click", () => {
+        let numeroTarjeta = "";
+
+        for(let i = 0; i < 16, i++){
+            
+        }
+
+
+    });
+
+
 
 }
 
