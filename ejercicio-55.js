504183ce3912ab11d220aec1d2596e36c4da3864
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-19 16:27:09 -0300

    Agregando ej 55

---
+++ ejercicio-55.js
@@ -0,0 +1,15 @@
+
+
+
+//55. Calculadora de propinas: Crea una calculadora que calcule automáticamente el monto 
+//de la propina en función del porcentaje deseado y el monto de la cuenta.
+
+
+
+
+
+
+
+
+
+
