4590058b61c108e138a9224a051e5119ef7be60c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-21 00:08:25 -0300

    Agregando ejercicio 56

---
+++ ejercicio-56.js
@@ -0,0 +1,14 @@
+
+
+
+//56. Hacer como una cuenta regresiva con distintos codigos como x ejemplo de whatsapp 
+//cuando pedis el numero para vincualr y te nmandan un codigo por 2 minutos
+
+
+export default function codigoRegresivo(){
+
+    
+
+
+}
+
