3710033165bababd5985f1db253f33c46a06e93f
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-21 00:02:47 -0300

    Documentando ej 55

--- ejercicio-55.js
+++ ejercicio-55.js
@@ -6,27 +6,34 @@
 
 export default function calculadoraPropinas(inputMonto, inputPorcentaje, btnCalcular, btnLimpiar, resultado){
 
-
+    //creando variables referenciales al DOM
     const $btnCalcular = document.querySelector(btnCalcular);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //captando evento click del boton calcular
     $btnCalcular.addEventListener("click", () => {
 
+        //tomando el valor de los input del dom escritos por el usuario
         const $inputMonto = document.querySelector(inputMonto).value;
         const $inputPorcentaje = document.querySelector(inputPorcentaje).value;
+        //creando variable propina que guardara el valor del calculo para la propina
         let propina = 0;
 
+        //calculando el valor de la propina
         propina = $inputPorcentaje * $inputMonto / 100;
 
-        console.log(propina);
+        //console.log(propina);
 
+        //mostrando resultado en DOM
         $resultado.textContent = `La propina debe ser: $${propina}`
         //console.log($inputMonto, $inputPorcentaje);
 
     });
 
+    //captando evento click del boton limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //Limpiando el resultado en el DOM
         $resultado.textContent = "";
     });
 
