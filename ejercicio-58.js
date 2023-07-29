eefd2aa69de02277ebda169c5679c7a8498ab74b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-29 17:52:06 -0300

    Documentando ej 58

--- ejercicio-58.js
+++ ejercicio-58.js
@@ -5,14 +5,18 @@
 
 export default function carritoCompras(btnAgregar1, btnAgregar2, btnAgregar3, btnDescuento1, btnDescuento2, btnDescuento3, resultado, btnLimpiar){
 
+    //Creando variable referencial al DOM de resultado
     const $resultado = document.querySelector(resultado);
 
+    //creando variables que seran utilizadas para calcular descuentos y total
     let total = 0;
     let cantidad = 0;
     let calculoDesc = 0;
 
+    //Captando evento click del DOM
     document.addEventListener("click", (e) => {
 
+        //Si coincide con algun boton agregar, sumarlo al monto total y 1 mas de cantidad
         if(e.target.matches(btnAgregar1)){
             total = total + 190;
             cantidad++;
@@ -26,10 +30,12 @@ export default function carritoCompras(btnAgregar1, btnAgregar2, btnAgregar3, bt
             cantidad++;
         }
 
+        //Actualizando la cantidad de celulares en la tabla del DOM
         document.querySelector(".cantidad-ej-58").textContent = `${cantidad}`;
 
         //console.log(total);
         //console.log(cantidad);
+        //Si coincide con algun boton descuento, que haga el calculo especifico del boton
         if(e.target.matches(btnDescuento1)){
             calculoDesc = total * 10 / 100;
             total = total - calculoDesc;
@@ -46,9 +52,13 @@ export default function carritoCompras(btnAgregar1, btnAgregar2, btnAgregar3, bt
             //console.log(total);
         }
 
+        //Mostrando resultado en el DOM
         $resultado.textContent = `El total es: $${total}`;
 
+        //Si el evento click coincide con el boton limpiar
         if(e.target.matches(btnLimpiar)){
+            //Limpia el resultado del DOM, la cantidad en la tabla y vuelve las variables
+            //iniciales a 0
             $resultado.textContent = " ";
             document.querySelector(".cantidad-ej-58").textContent = " ";
             total = 0;
