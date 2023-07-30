520969f9a3f6df2fe1774d3b0c20cc9edd14dd44
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-30 03:11:59 -0300

    Agregando ej 59

---
+++ ejercicio-59.js
@@ -0,0 +1,19 @@
+
+
+//59. Validación de tarjeta de crédito: Escribe una función que tome un número de tarjeta 
+//de crédito como entrada y verifique si es un número de tarjeta de crédito válido 
+//utilizando el algoritmo de Luhn.
+
+
+export default function validacionTarjetaCredito(){
+
+    
+
+}
+
+
+
+
+
+
+
