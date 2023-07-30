02778840563e089621b9a630f3a0b693e02ccc0d
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-30 15:04:40 -0300

    Documentando ej 59

--- ejercicio-59.js
+++ ejercicio-59.js
@@ -7,20 +7,25 @@
 
 export default function validacionTarjetaCredito(inputNro, btnValidar, btnLimpiar, resultado){
 
+    //creando la expresion regular con el formato valido de las tarjetas
     const formato = /^[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}$/
 
+    //creando variables referenciales al DOM
     const $btnValidar = document.querySelector(btnValidar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
 
+    //Captando evento click del boton Validar
     $btnValidar.addEventListener("click", () => {
 
+        //tomando el valor del input escrito por el usuario
         const $inputNro = document.querySelector(inputNro).value;
 
 
         //console.log($inputNro);
 
+        //validando el input ingrsado a ver si coincide con la expresion regular.
         if(formato.test($inputNro)){
             //console.log("ASD");
             $resultado.textContent = "Formato Valido!";
@@ -31,7 +36,9 @@ export default function validacionTarjetaCredito(inputNro, btnValidar, btnLimpia
 
     });
 
+    //Captando evento click del evento Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //Limpiando el resultado en el DOM y el input
         $resultado.textContent = " ";
         document.querySelector(inputNro).value = " ";
     });
