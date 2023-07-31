4023f60111777d48b95fdbc5f75646e8c5157c7a
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-31 16:40:48 -0300

    Creando contrasenia en ej 60

--- ejercicio-60.js
+++ ejercicio-60.js
@@ -12,18 +12,24 @@ export default function generadorContraseniaAvanzado(btnGenerar, btnLimpiar, res
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
-    const numero = /[0-9]+/;
-    const letra = /[a-zA-Z]+/;
-    const simbolo = /[\W_]+/;
+    const numero = [0,1,2,3,4,5,6,7,8,9];
+    const letra = ["A","B","C","D","E",
+    "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z","a","b","c",
+    "d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","r","x","w","y","z"];;
+    const simbolo = ["!","@","#","$","%","&","/","(",")","=","~","°","{","[","}","]","Ç","|"];
 
     let contrasenia = "";
 
     $btnGnerar.addEventListener("click", () => {
         
         for(let i = 0; i < 13; i++){
-            
+            contrasenia += numero[Math.floor(Math.random() * 10)];
+            contrasenia += letra[Math.floor(Math.random() * 48)];
+            contrasenia += simbolo[Math.floor(Math.random() * 18)];
         }
 
+        console.log(contrasenia);
+        
     });
 
 
