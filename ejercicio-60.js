e816c7d82ec7d12f43158b8df679188ab4c06160
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-31 17:11:39 -0300

    Logica resuelta de caracteres consecutivos del mismo tipo ej 60

--- ejercicio-60.js
+++ ejercicio-60.js
@@ -11,13 +11,6 @@ export default function generadorContraseniaAvanzado(btnGenerar, btnLimpiar, res
     const $btnGnerar = document.querySelector(btnGenerar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
-    /*
-    const numero = [0,1,2,3,4,5,6,7,8,9];
-    const letra = ["A","B","C","D","E",
-    "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z","a","b","c",
-    "d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","r","x","w","y","z"];
-    const simbolo = ["!","@","#","$","%","&","/","(",")","=","~","°","{","[","}","]","Ç","|"];
-*/
 
     const objeto = {
         numero: [0,1,2,3,4,5,6,7,8,9],
@@ -32,12 +25,16 @@ export default function generadorContraseniaAvanzado(btnGenerar, btnLimpiar, res
     const tipo = ["numero", "letra", "simbolo"];
 
     $btnGnerar.addEventListener("click", () => {
-        
+        let nuevoIterador = "";
+
         for(let i = 0; i < 12; i++){
-            const iterador = tipo[Math.floor(Math.random() * tipo.length)];
+            let iterador = tipo[Math.floor(Math.random() * tipo.length)];
+
+            do{
+                iterador = tipo[Math.floor(Math.random() * tipo.length)];    
+            }while(iterador === nuevoIterador);
 
             let caracterAleatorio = "";
-
         
             switch(iterador){
                 case "numero":
@@ -52,6 +49,7 @@ export default function generadorContraseniaAvanzado(btnGenerar, btnLimpiar, res
             }
             
             contrasenia += caracterAleatorio;
+            nuevoIterador = iterador;
 
         }
 
