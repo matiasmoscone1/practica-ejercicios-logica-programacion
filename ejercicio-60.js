ade146a7cf18948c9db97416090505b4b7113d81
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-07-31 16:59:55 -0300

    Resolviendo logica de contrasenia

--- ejercicio-60.js
+++ ejercicio-60.js
@@ -11,21 +11,48 @@ export default function generadorContraseniaAvanzado(btnGenerar, btnLimpiar, res
     const $btnGnerar = document.querySelector(btnGenerar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
-
+    /*
     const numero = [0,1,2,3,4,5,6,7,8,9];
     const letra = ["A","B","C","D","E",
     "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z","a","b","c",
-    "d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","r","x","w","y","z"];;
+    "d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","r","x","w","y","z"];
     const simbolo = ["!","@","#","$","%","&","/","(",")","=","~","°","{","[","}","]","Ç","|"];
+*/
+
+    const objeto = {
+        numero: [0,1,2,3,4,5,6,7,8,9],
+        letra: ["A","B","C","D","E",
+        "F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","W","X","Y","Z","a","b","c",
+        "d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","r","x","w","y","z"],
+        simbolo: ["!","@","#","$","%","&","/","(",")","=","~","°","{","[","}","]","Ç","|"]
+    }
 
     let contrasenia = "";
 
+    const tipo = ["numero", "letra", "simbolo"];
+
     $btnGnerar.addEventListener("click", () => {
         
-        for(let i = 0; i < 13; i++){
-            contrasenia += numero[Math.floor(Math.random() * 10)];
-            contrasenia += letra[Math.floor(Math.random() * 48)];
-            contrasenia += simbolo[Math.floor(Math.random() * 18)];
+        for(let i = 0; i < 12; i++){
+            const iterador = tipo[Math.floor(Math.random() * tipo.length)];
+
+            let caracterAleatorio = "";
+
+        
+            switch(iterador){
+                case "numero":
+                    caracterAleatorio = objeto.numero[Math.floor(Math.random() * 10)];
+                    break;
+                case "letra":
+                    caracterAleatorio = objeto.letra[Math.floor(Math.random() * 48)];
+                    break;
+                case "simbolo":
+                    caracterAleatorio = objeto.simbolo[Math.floor(Math.random() * 18)];
+                    break;    
+            }
+            
+            contrasenia += caracterAleatorio;
+
         }
 
         console.log(contrasenia);
