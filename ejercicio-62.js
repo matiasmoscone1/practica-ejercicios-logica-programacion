be53503f9dfe076229d9e19e7e8bba0e30e7611b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-03 18:36:24 -0300

    Implementando validacion de email ej 62

--- ejercicio-62.js
+++ ejercicio-62.js
@@ -13,14 +13,17 @@ export default function validarFormulario(btnValidar, btnLimpiar, nombre, apelli
     const letra = /[a-zA-Z]+/;
     const numero = /[0-9]+/;
     const simbolo = /[\W_]+/;
+    const regEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z-.]+\.[a-zA-Z]{3}$/;
 
+    
+    
     const validarNombre = (nom) => {
         if(numero.test(nom) || simbolo.test(nom) || nom.length < 3){
             console.log("Ingrese un nombre valido (solo letras)...");
         }else{
             console.log("Nombre valido!");
         }
-    }
+    };
     
     const validarApellido = (ape) => {
         if(numero.test(ape) || simbolo.test(ape) || ape.length < 3){
@@ -28,17 +31,26 @@ export default function validarFormulario(btnValidar, btnLimpiar, nombre, apelli
         }else{
             console.log("Apellido valido!");
         }
-    }
+    };
+
+    const validarEmail = (ema) => {
+        if(regEmail.test(ema)){
+            console.log("Email valido!");
+        }else{
+            console.log("Ingrese un email valido (xxxx@xxxx.xxx)");
+        }
+
+    };
 
 
     $btnValidar.addEventListener("click", () => {
         const $nombre = document.querySelector(nombre).value;
         const $apellido = document.querySelector(apellido).value;
-
+        const $email = document.querySelector(email).value;
 
         validarNombre($nombre);
         validarApellido($apellido);
-
+        validarEmail($email);
 
 
     });
