4f3e45fc0f39d44ad27c5ff24aad4058f3cb9176
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-03 17:38:47 -0300

    Implementando logica de validacion para nombre y apellido

--- ejercicio-62.js
+++ ejercicio-62.js
@@ -5,11 +5,33 @@
 //campos, como dirección de correo electrónico, contraseña segura, número de teléfono, etc.
 
 
-export default function validarFormulario(){
+export default function validarFormulario(btnValidar, btnLimpiar, nombre, apellido, email, fechaNac, resultado){
+
+    const $btnValidar = document.querySelector(btnValidar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+
+    const letra = /[a-zA-Z]+/;
+    const numero = /[0-9]+/;
+    const simbolo = /[\W_]+/;
+
+    const validarNombre = (nom) => {
+        if(numero.test(nom) || numero.test(simbolo)){
+            console.log("Ingrese un nombre valido (solo letras)...");
+        }else{
+            console.log("Nombre valido!");
+        }
+    }
+
+
+    $btnValidar.addEventListener("click", () => {
+
+        validarNombre(document.querySelector(nombre).value);
 
 
 
-    
+    });
+
+
 }
 
 
