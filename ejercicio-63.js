0049b67b891f1e6ef4397376f1a86ff8c77fec17
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-04 17:27:03 -0300

    Ej 63 maquetado, falta implementar logica

--- ejercicio-63.js
+++ ejercicio-63.js
@@ -4,10 +4,16 @@
 //63.Calculadora de precios de envío: Crea una calculadora que estime el precio de envío 
 //de un paquete según el peso, tamaño y destino.
 // 10kg < $500
-// 10kg > 50kg < $800
-// 50kg < 100kg < $1300
+// 10kg > $800
+// 50kg > 100kg < $1300
 
+//Pequeño - $200
+//Mediano - $500
+//Grande - $800
 
+// 10km < $400
+// 10km > $700
+// 50km > $1100
 
 
 export default function calcularEnvio(){
