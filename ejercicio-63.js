60d14b98b685579d7e33412d57e3e89705fafe87
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-05 02:47:54 -0300

    Resolviendo ej 63

--- ejercicio-63.js
+++ ejercicio-63.js
@@ -3,22 +3,48 @@
 
 //63.Calculadora de precios de envío: Crea una calculadora que estime el precio de envío 
 //de un paquete según el peso, tamaño y destino.
-// 10kg < $500
-// 10kg > $800
-// 50kg > 100kg < $1300
+// liviano < $500
+// mediano > $800
+// pesado > 100kg < $1300
 
 //Pequeño - $200
 //Mediano - $500
 //Grande - $800
 
-// 10km < $400
-// 10km > $700
-// 50km > $1100
+// cerca < $400
+//  mitad > $700
+// lejos > $1100
 
 
 export default function calcularEnvio(btnCalcular, btnLimpiar, peso, tamano, distancia, resultado){
 
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
 
+    $btnCalcular.addEventListener("click", () => {
+        const $peso = document.querySelector(peso).value;
+        const $tamano = document.querySelector(tamano).value;
+        const $distancia = document.querySelector(distancia).value;
+
+        const objPeso = {
+            liviano: 500,
+            mediano: 800,
+            pesado: 1300
+        };
+        const objTamano = {
+            pequeno: 200,
+            mediano: 500,
+            grande: 800
+        };
+        const objDistancia = {
+            cerca: 400,
+            mitad: 700,
+            lejos: 1100
+        };
+        
+
+
+    });
 
 
 
