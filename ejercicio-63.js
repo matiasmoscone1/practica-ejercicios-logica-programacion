43292cdc13d9b2c2a31883b7f2c64ce3d346c7a8
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-07 20:27:54 -0300

    Documentando ejercicio 63

--- ejercicio-63.js
+++ ejercicio-63.js
@@ -18,49 +18,56 @@
 
 export default function calcularEnvio(btnCalcular, btnLimpiar, peso, tamano, distancia, resultado){
 
+    //Creando variables referenciales al DOM
     const $btnCalcular = document.querySelector(btnCalcular);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //captando evento click del boton Calcular
     $btnCalcular.addEventListener("click", () => {
+        //tomando el valor de los input escritos por el usuario
         const $peso = document.querySelector(peso).value;
         const $tamano = document.querySelector(tamano).value;
         const $distancia = document.querySelector(distancia).value;
 
+        //Creando objeto Peso
         const objPeso = {
             liviano: 500,
             mediano: 800,
             pesado: 1300
         };
+        //Creando objeto Tamano
         const objTamano = {
             pequeno: 200,
             mediano: 500,
             grande: 800
         };
+        //Creando objeto Distancia
         const objDistancia = {
             cerca: 400,
             mitad: 700,
             lejos: 1100
         };
 
+        //Creando variables que almacenaran el valor de la propiedad de cada objeto, dado
+        //a lo escrito por el usuario en los respectivos inputs 
         const costoPeso = objPeso[$peso];
         const costoTamano = objTamano[$tamano];
         const costoDistancia = objDistancia[$distancia];
 
+        //Calcula el costo total con las variables creadas anteriormente
         let costoTotal = costoPeso + costoTamano + costoDistancia;
 
+        //Muestra el resultado en el DOM
         $resultado.textContent = `El costo total del envio es: $${costoTotal}`;
-
-
-        //console.log($peso);
-        //console.log($tamano);
-        //console.log($distancia);
-        //console.log(costoTotal);
 
     });
 
+    //captando el evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //Limpia el resultado en el DOM
         $resultado.textContent = " ";
+        //Limpia el valor de los inputs 
         document.querySelector(peso).value = " ";
         document.querySelector(tamano).value = " ";
         document.querySelector(distancia) = " ";
