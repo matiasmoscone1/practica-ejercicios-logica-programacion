22524a43b864e8f7e1890afbc65705af4302e2da
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-08 19:20:08 -0300

    Documentando ej 64

--- ejercicio-64.js
+++ ejercicio-64.js
@@ -7,32 +7,37 @@
 
 export default function tarjetaPresentacion(btnGenerar, btnLimpiar, inputNombre, inputApellido, inputTelefono, inputEmail, inputOcupacion, resultado){
 
-
+    //creando variables referenciales al DOM
     const $btnGenerar = document.querySelector(btnGenerar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
     
+    //captando evento click del boton Generar
     $btnGenerar.addEventListener("click", () => {
+        //tomando el valor de los input escritos por el usuario
         const $inputNombre = document.querySelector(inputNombre).value;
         const $inputApellido = document.querySelector(inputApellido).value;
         const $inputTelefono = document.querySelector(inputTelefono).value;
         const $inputEmail = document.querySelector(inputEmail).value;
         const $inputOcupacion = document.querySelector(inputOcupacion).value;        
 
-
+        //Mostrando el resultado en el DOM y creando la carta de presentacion.
         $resultado.innerHTML = `Hola soy ${$inputNombre} ${$inputApellido}, me dedico a 
         ${$inputOcupacion}. Me pueden contactar a traves de mi mail: ${$inputEmail} o a traves
         de mi telefono: ${$inputTelefono}. Saludos cordiales!`;
         
     });
 
+    //Captando evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //Limpia todos los inputs 
         document.querySelector(inputNombre).value = " ";
         document.querySelector(inputApellido).value = " ";
         document.querySelector(inputTelefono).value = " ";
         document.querySelector(inputEmail).value = " ";
         document.querySelector(inputOcupacion).value = " ";   
+        //limpia el resultado en el DOM
         $resultado.innerHTML = " ";        
     });
 
