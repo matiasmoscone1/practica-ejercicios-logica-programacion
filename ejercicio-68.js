f9de25853aaf07057631e2a20513e17777b4750f
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-11 20:03:49 -0300

    Intentando resolver logica de calculadora de ej 68

--- ejercicio-68.js
+++ ejercicio-68.js
@@ -8,19 +8,25 @@
 export default function calculadoraInteractiva(divCalc ,suma, resta, multi, divi, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, cero, resultado){
     const $divCalc = document.querySelector(divCalc);
     const $resultado = document.querySelector(resultado);
-
+/*
     const $suma = document.querySelector(suma);
     const $resta = document.querySelector(resta);
     const $multi = document.querySelector(multi);
     const $divi = document.querySelector(divi);
-
+*/
+   
     let primerNum = "0";
     let segundoNum = "0";
 
-
     $divCalc.addEventListener("click", (e) => {
+
+        
         if(e.target.matches(uno)){
             primerNum += 1;
+            parseInt(primerNum);
+        }
+        if(e.target.matches(dos)){
+            primerNum += 2;
             parseInt(primerNum);
         }
 
@@ -28,7 +34,27 @@ export default function calculadoraInteractiva(divCalc ,suma, resta, multi, divi
         $resultado.textContent = `${parseInt(primerNum)}`;
 
         console.log(typeof(parseInt(primerNum)));
+
+        if(e.target.matches(suma)){
+            if(primerNum){
+                $divCalc.addEventListener("click", (e) => {
+                    if(e.target.matches(uno)){
+                        segundoNum += 1;
+                        parseInt(segundoNum);
+                    }
+                    if(e.target.matches(dos)){
+                        segundoNum += 2;
+                        parseInt(segundoNum);
+                    }
+                });
+            }
+            $resultado.textContent = `${primerNum} + ${segundoNum}`;
+        }
+
+
     });
+
+   
 
 
 
