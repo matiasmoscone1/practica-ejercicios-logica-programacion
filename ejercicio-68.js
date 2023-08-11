ffa30a42dee8242020c73018ba6bcb596cbdf0d9
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-11 19:52:34 -0300

    Resolviendo ej 68

--- ejercicio-68.js
+++ ejercicio-68.js
@@ -5,11 +5,33 @@
 //clic en los botones numéricos y operadores para realizar cálculos. Muestra los 
 //resultados en tiempo real.
 
-export default function calculadoraInteractiva(suma, resta, multi, divi, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, cero, resultado){
-
-
-
+export default function calculadoraInteractiva(divCalc ,suma, resta, multi, divi, uno, dos, tres, cuatro, cinco, seis, siete, ocho, nueve, cero, resultado){
+    const $divCalc = document.querySelector(divCalc);
     const $resultado = document.querySelector(resultado);
+
+    const $suma = document.querySelector(suma);
+    const $resta = document.querySelector(resta);
+    const $multi = document.querySelector(multi);
+    const $divi = document.querySelector(divi);
+
+    let primerNum = "0";
+    let segundoNum = "0";
+
+
+    $divCalc.addEventListener("click", (e) => {
+        if(e.target.matches(uno)){
+            primerNum += 1;
+            parseInt(primerNum);
+        }
+
+
+        $resultado.textContent = `${parseInt(primerNum)}`;
+
+        console.log(typeof(parseInt(primerNum)));
+    });
+
+
+
 
 }
 
