c9daeb81397e10f2b58e44fed8a150f1c4f6b6a9
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-15 00:00:51 -0300

    Resolviendo ej 69

--- ejercicio-69.js
+++ ejercicio-69.js
@@ -3,9 +3,15 @@
 //69. Captura y muestra la ubicación del mouse: Crea una página que muestre las 
 //coordenadas (X, Y) del mouse en tiempo real mientras se mueve sobre ella.
 
-export default function mostrarCoordenadasMouse(btnMostrar, btnOcultar){
+export default function mostrarCoordenadasMouse(btnMostrar, btnOcultar, resultado){
 
     
+    const $btnMostrar = document.querySelector(btnMostrar);
+    const $btnOcultar = document.querySelector(btnOcultar);
+    const $resultado = document.querySelector(resultado);
+    
+    $btnMostrar.$btnMostrar
+
 
 
 }
