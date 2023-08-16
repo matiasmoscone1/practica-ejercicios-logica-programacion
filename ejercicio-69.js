401bce9d996e16784f09557537ae024b542301c8
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-16 17:42:36 -0300

    Documentando ej 69

--- ejercicio-69.js
+++ ejercicio-69.js
@@ -5,15 +5,18 @@
 
 export default function mostrarCoordenadasMouse(btnMostrar, btnOcultar, resultadoX, resultadoY){
 
-    
+    //Creando las variables referenciales al DOM y moverMouse
     const $btnMostrar = document.querySelector(btnMostrar);
     const $btnOcultar = document.querySelector(btnOcultar);
     const $resultadoX = document.querySelector(resultadoX);
     const $resultadoY = document.querySelector(resultadoY);
     let moverMouse = null;
 
+    //captando evento click del boton Mostrar
     $btnMostrar.addEventListener("click", () => {
 
+        //guarda en variable moverMouse la callback que actualizara en todo momento los
+        //resultados del movimiento del mouse
         moverMouse = (e) => {
             $resultadoX.textContent = `X: ${e.clientX}`;
             $resultadoY.textContent = `Y: ${e.clientY}`;
@@ -21,14 +24,18 @@ export default function mostrarCoordenadasMouse(btnMostrar, btnOcultar, resultad
             //console.log(e.clientY);
         }
 
+        //capta el evento mousemove y le pasa la variable moverMouse con su respectiva callback
         document.addEventListener("mousemove", moverMouse);
 
     });
 
+    //capta evento click del boton Ocultar
     $btnOcultar.addEventListener("click", () => {
+        //si existe moverMouse, se le remueve el evento
         if(moverMouse){
             document.removeEventListener("mousemove", moverMouse);
         }
+        //borra los resultados en el DOM
         $resultadoX.textContent = " ";
         $resultadoY.textContent = " ";
     });
