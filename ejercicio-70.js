7ea7adf64a0d1d12442b3e2e19ef8dbd8cd36bb8
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-17 18:03:16 -0300

    Resolviendo logica de autocompletado en ej 70

--- ejercicio-70.js
+++ ejercicio-70.js
@@ -9,12 +9,47 @@ export default function autocompletar(texto){
 
     const $texto = document.querySelector(texto);
 
+    const provincias = ["Buenos Aires",
+    "Ciudad Autónoma de Buenos Aires",
+    "Catamarca",
+    "Chaco",
+    "Chubut",
+    "Córdoba",
+    "Corrientes",
+    "Entre Ríos",
+    "Formosa",
+    "Jujuy",
+    "La Pampa",
+    "La Rioja",
+    "Mendoza",
+    "Misiones",
+    "Neuquén",
+    "Río Negro",
+    "Salta",
+    "San Juan",
+    "San Luis",
+    "Santa Cruz",
+    "Santa Fe",
+    "Santiago del Estero",
+    "Tierra del Fuego, Antártida e Islas del Atlántico Sur",
+    "Tucumán"];
+
+
+    const filtarOpciones = (cadenaTexto) => {
+        return provincias.filter(provincia => provincia.toLowerCase().startsWith(cadenaTexto.toLowerCase()));
+    }
+
 
     $texto.addEventListener("keyup", (e) => {
 
         let valorTexto = "";
         valorTexto += e.target.value;
         console.log(valorTexto);
+
+        const prueba = filtarOpciones(valorTexto);
+        console.log(prueba);
+
+        
 
 
     });
