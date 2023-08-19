0d5691c541c2cf2542fea6ac36a6299e7e23032e
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-19 18:41:57 -0300

    Ejercicio 71 documentado

--- ejercicio-71.js
+++ ejercicio-71.js
@@ -32,39 +32,51 @@ export default function temporizadorRegresivo(btnInicio, btnLimpiar, inputMin, i
         //creando funcion callback que le pasa por parametro el tiempo total en segundos
         const actualizarCronometro = (tiempo) => {
 
-            //
+            //creando variable minutos que se calcula con el tiempo total dividido en 60
             let minutos = tiempo / 60;
+            //calcula la parte decimal restante del calculo de la variable minutos
             let parteDecimalMinutos = minutos - Math.floor(minutos);
             //console.log(parteDecimalMinutos);
+            //creando variable segundos que redondea el resultado de la variable parteDecimalMinutos por 60
             let segundos = Math.round(parteDecimalMinutos * 60);
 
+            //agrega un 0 adelante para la interfaz, cuando "segundos" es menor a 10
             if(segundos < 10){
                 segundos = `0${segundos}`;
             }
-            console.log("Minutos:", Math.floor(minutos));
-            console.log("Segundos:", segundos);
+            //console.log("Minutos:", Math.floor(minutos));
+            //console.log("Segundos:", segundos);
 
+            //muestra resultado en el DOM
             $resultado.textContent = `${Math.floor(minutos)}:${segundos}`;
             
         }
 
+        //funcion callback que le resta 1 al tiempo total
         const intervalo = () => {
             tiempoTotal--;
 
             actualizarCronometro(tiempoTotal);
-            console.log(tiempoTotal);
+            //console.log(tiempoTotal);
+            //cierra el intervalo cuando se termina la cuenta regresiva
             if(tiempoTotal === 0){
                 clearInterval(tempIntervalo);
-                console.log("CRONOMETRO TERMINADO");
+                $resultado.textContent = "CRONOMETRO TERMINADO!";
+                //console.log("CRONOMETRO TERMINADO");
             }
         }
 
-
+        //Guarda en una variable temporal el intervalo generado para luego poder cerrarlo
         const tempIntervalo = setInterval(intervalo, 1000);
+
+        //capta el evento click del boton limpiar
         $btnLimpiar.addEventListener("click", () => {
+            //cierra el intervalo
             clearInterval(tempIntervalo);
+            //deja los inputs vacios
             document.querySelector(inputMin).value = " ";
             document.querySelector(inputSeg).value = " ";
+            //limpia el resultado del dom
             $resultado.textContent = " ";
         });
 
