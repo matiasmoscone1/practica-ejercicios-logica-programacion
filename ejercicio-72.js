90a5bf8651f3f620bd8cd20e95772d8edb590bbc
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-19 18:52:49 -0300

    Agregando ejercicio 72

---
+++ ejercicio-72.js
@@ -0,0 +1,18 @@
+
+
+
+
+//72. Crea una función que tome un objeto y devuelva un nuevo objeto con todas las 
+//claves y valores intercambiados.
+
+
+export default function cambiarApellidos(){
+
+
+
+    
+}
+
+
+
+
