6623de1b7118d70b2638c8a4da08751965243289
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-24 18:50:16 -0300

    Resolviendo ej 73

--- ejercicio-73.js
+++ ejercicio-73.js
@@ -3,11 +3,28 @@
 //73. Capta determinados tipos de inputs en el documento y los guarda en un conjunto.
 
 
-export default function valorInputs(){
+export default function valorInputs(btnInicio, btnFinalizar){
 
-
-
+    const $btnInicio = document.querySelector(btnInicio);
+    const $btnFinalizar = document.querySelector(btnFinalizar);
     
+
+    $btnInicio.addEventListener("click", () => {
+
+        let guardaCaracteres;
+
+        document.addEventListener("keyup", (e) => {
+
+            console.log(e);
+
+        });
+
+    });
+
+
+
+
+
 }
 
 
--- index.js
+++ index.js
@@ -147,8 +147,8 @@ document.addEventListener("DOMContentLoaded", () => {
     autocompletar(".input-ej-70", ".lista-ej-70");
     temporizadorRegresivo(".btn-inicio-ej-71", ".btn-limpiar-ej-71", ".input-minutos-ej-71", ".input-segundos-ej-71", ".resultado-ej-71");
     cambiarValores(".div-objeto-ej-72", ".btn-cambiar-ej-72", ".btn-limpiar-ej-72");
-    valorInputs();
-    
+    valorInputs(".btn-inicio-ej-73", ".btn-finalizar-ej-73");
+
 
 
 });
