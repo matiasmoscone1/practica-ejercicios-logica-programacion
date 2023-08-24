4153c09013c669e2d075a8b4a421dc6a1d302532
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-24 19:23:06 -0300

    Agregando ejercicio 74

---
+++ ejercicio-74.js
@@ -0,0 +1,18 @@
+
+
+//74. Lo mismo que el ejercicio 73 pero esta vez cuando se envie el formulario de carga
+//que guarde el valor del input de tipo contraseña. (no todas las letras presionadas)
+
+
+export default function valorInputsFormulario(){
+
+
+    
+}
+
+
+
+
+
+
+
--- index.js
+++ index.js
@@ -71,6 +71,7 @@ import autocompletar from "./ejercicio-70.js";
 import temporizadorRegresivo from "./ejercicio-71.js";
 import cambiarValores from "./ejercicio-72.js";
 import valorInputs from "./ejercicio-73.js";
+import valorInputsFormulario from "./ejercicio-74.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -148,6 +149,7 @@ document.addEventListener("DOMContentLoaded", () => {
     temporizadorRegresivo(".btn-inicio-ej-71", ".btn-limpiar-ej-71", ".input-minutos-ej-71", ".input-segundos-ej-71", ".resultado-ej-71");
     cambiarValores(".div-objeto-ej-72", ".btn-cambiar-ej-72", ".btn-limpiar-ej-72");
     valorInputs(".btn-inicio-ej-73", ".btn-finalizar-ej-73");
+    valorInputsFormulario(".btn-inicio-ej-74", ".btn-finalizar-ej-74");
 
 
 
