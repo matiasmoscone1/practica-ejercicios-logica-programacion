e04e5fe316a449b489d82179bd5926f62a729068
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-24 19:28:29 -0300

    Documentando ej 73

--- ejercicio-73.js
+++ ejercicio-73.js
@@ -5,30 +5,43 @@
 
 export default function valorInputs(btnInicio, btnFinalizar){
 
+    //Creando variables referenciales al DOM
     const $btnInicio = document.querySelector(btnInicio);
     const $btnFinalizar = document.querySelector(btnFinalizar);
     
 
+    //captando evento click del boton Inicio
     $btnInicio.addEventListener("click", () => {
 
+        //creando variable que contendra las teclas presionadas en el input tipo password
         let guardaCaracteres = "";
 
+        //creando funcion callback que pasa como parametro el evento
         const keyUp = (e) => {
+            //verifica si tagName del evento es un input y si el tipo del input es "password"
             if(e.target.tagName === "INPUT" && e.target.type === "password"){
                 //console.log("HOLA!");
+                //guarda las teclas presionadas en la variable creada anteriormente
                 guardaCaracteres += e.target.value;
             }
+            //muestra en consola la cadena de caracteres presionados en el input tipo password
             console.log(guardaCaracteres);
         }
 
+        //capta evento keyup del documento y le pasa como parametro la callback creada
         document.addEventListener("keyup", keyUp);
-        
+
+
+        //capta evento click del boton Finalizar
+        $btnFinalizar.addEventListener("click", () => {
+            //remueve el evento escuchado (en este caso el keyup captado)
+            document.removeEventListener("keyup", keyUp);
+            
+        });
+
     });
 
-    $btnFinalizar.addEventListener("click", () => {
-        document.removeEventListener("keyup", keyUp);
-
-    });
+   
 
 
 
