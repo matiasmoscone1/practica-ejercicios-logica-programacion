b2652e2d59c99d100b143563de7d2910f56e5f6a
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-27 17:29:37 -0300

    Ejercicio 75 terminado, ultimando detalles

--- ejercicio-75.js
+++ ejercicio-75.js
@@ -4,7 +4,11 @@
 //75. Implementa una función recursiva para calcular el factorial de un número.
 
 
-export default function factRecursiva(){
+export default function factRecursiva(btnCalcular, btnLimpiar, input, resultado){
+
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
 
     const factNum = (num) => {
         if(num === 0 || num === 1){
@@ -16,7 +20,18 @@ export default function factRecursiva(){
     }
 
 
-    console.log(factNum(5));
+    $btnCalcular.addEventListener("click", () => {
+        const $input = document.querySelector(input).value;
+
+        const res = factNum($input);
+
+        $resultado.textContent = `Resultado: ${res}`
+
+    });
+
+
+
+    //console.log(factNum(5));
 
 }
 
