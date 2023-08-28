a40118a699707a8a6d72d8cb14e4d8db062211ef
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-28 18:25:05 -0300

    Resolviendo ej 76

--- ejercicio-76.js
+++ ejercicio-76.js
@@ -5,7 +5,19 @@
 
 export default function busquedaBinaria(btnBuscar, btnLimpiar, numero, resultado){
 
+
+    const $btnBuscar = document.querySelector(btnBuscar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
     
+    const arrayNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    let izquierda = 0;
+    let derecha = arrayNum.length - 1;
+
+    
+
+
 
 }
 
