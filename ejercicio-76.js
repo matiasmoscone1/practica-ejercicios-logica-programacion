466b85a6a885dbdb283955e25b77721120656175
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-08-28 18:32:01 -0300

    Resolviendo logica del ej 76

--- ejercicio-76.js
+++ ejercicio-76.js
@@ -10,12 +10,29 @@ export default function busquedaBinaria(btnBuscar, btnLimpiar, numero, resultado
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
     
-    const arrayNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-    let izquierda = 0;
-    let derecha = arrayNum.length - 1;
+    $btnBuscar.addEventListener("click", () => {
+        const $numero = document.querySelector(numero).value;
 
-    
+        const arrayNum = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+        let izquierda = 0;
+        let derecha = arrayNum.length - 1;
+
+        while(izquierda <= derecha){
+            let medio = Math.floor((izquierda + derecha) / 2);
+
+            if(arrayNum[medio] === $numero){
+                console.log(`El numero encontrado es: ${medio}`);
+            }
+            else if(arrayNum[medio] > $numero){
+                izquierda = medio + 1;    
+            }else{
+                derecha = medio - 1;
+            }
+        }
+
+    });
 
 
 
