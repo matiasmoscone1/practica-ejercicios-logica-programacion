f8d523f8e9f9f807ccad77a8fc52a6a29fa7ee4c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-02 14:11:49 -0300

    Documentando ej 77

--- ejercicio-77.js
+++ ejercicio-77.js
@@ -5,11 +5,14 @@
 
 export default function crearPersona(btnCrear, btnLimpiar, nombre, apellido, edad, resultado){
 
+    //creando variables referenciales al DOM
     const $btnCrear = document.querySelector(btnCrear);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //construyendo la clase persona
     class Persona{
+        //toma como parametros nombre(texto), apellido(texto), y edad(numero)
         constructor(nombre, apellido, edad){
             this.nombre = nombre;
             this.apellido = apellido;
@@ -17,15 +20,18 @@ export default function crearPersona(btnCrear, btnLimpiar, nombre, apellido, eda
         }
     }
 
-    
+    //captando evento click del boton Crear
     $btnCrear.addEventListener("click", () => {
+        //tomando los valores de los input del usuario
         const $nombre = document.querySelector(nombre).value;
         const $apellido = document.querySelector(apellido).value;
         const $edad = document.querySelector(edad).value;
 
-        
+        //crea una instancia de la clase Persona con los parametros de los valores de
+        //los inputs
         const instanciaPersona = new Persona($nombre, $apellido, $edad);
 
+        //Muestra en el DOM el resultado
         $resultado.innerHTML = `* Persona creada con exito * <br>
          Nombre: ${instanciaPersona.nombre} <br>
          Apellido: ${instanciaPersona.apellido} <br>
@@ -38,8 +44,9 @@ export default function crearPersona(btnCrear, btnLimpiar, nombre, apellido, eda
 
     });
 
-
+    //capta el evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //limpia el resultado y los valores de los input 
         $resultado.innerHTML = "";
         document.querySelector(nombre).value = "";
         document.querySelector(apellido).value = "";
