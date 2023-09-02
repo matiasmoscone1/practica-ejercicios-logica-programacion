989417b61a1ca1ceb323c3a9ad83810dd3207393
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-02 12:54:45 -0300

    Resolviendo ej 77

--- ejercicio-77.js
+++ ejercicio-77.js
@@ -5,8 +5,26 @@
 
 export default function crearPersona(btnCrear, btnLimpiar, nombre, apellido, edad, resultado){
 
+    const $btnCrear = document.querySelector(btnCrear);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+
+    class Persona{
+
+        constructor(nombre, apellido, edad){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.edad = edad;
+        }
+    }
 
     
+    $btnCrear.addEventListener("click", () => {
+        const $nombre = document.querySelector(nombre).value;
+
+        console.log($nombre);
+
+    });
+    const instanciaPersona = new Persona()
 
 
 
