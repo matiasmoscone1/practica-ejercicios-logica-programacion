c6a0147f029059109cd9bedec1b49425ed9188cb
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-06 19:52:17 -0300

    Resolviendo ej 80

--- ejercicio-80.js
+++ ejercicio-80.js
@@ -6,10 +6,29 @@
 export default function calculadoraClase(input1, input2, btnSuma, btnResta, btnMulti, btnDivi, resultado){
 
 
+    class Calculadora{
+        constructor(res){
+            this.res = res;
+        }
+
+        sumar(numero1, numero2){
+            this.res = parseFloat(numero1) + parseFloat(numero2);
+            return(console.log(this.res));
+        }
+
+    }
+
+
     document.addEventListener("click", (e) => {
+
+        const $input1 = document.querySelector(input1).value;
+        const $input2 = document.querySelector(input2).value;
+
+        const instancaiCalculadora = new Calculadora;
 
         if(e.target.matches(btnSuma)){
             console.log("hola");
+            instancaiCalculadora.sumar($input1, $input2);
         }else if(e.target.matches(btnResta)){
             console.log("resta");
         }else if(e.target.matches(btnMulti)){
