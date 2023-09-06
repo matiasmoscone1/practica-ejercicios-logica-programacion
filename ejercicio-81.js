e5233dbdb7477e4a0f8bb3cce2e05f5d9f624cb0
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-06 20:12:32 -0300

    Resolviendo ej 81

--- ejercicio-81.js
+++ ejercicio-81.js
@@ -3,10 +3,40 @@
 //81. Crea una clase Libro con propiedades para título, autor y año de publicación.
 
 
-export default function modificarLibro(){
+export default function modificarLibro(titulo, autor, anio, btnCrear, btnLimpiar, resultado){
+
+    const $btnCrear = document.querySelector(btnCrear);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+
+    class Libro{
+        constructor(titulo, autor, anio){
+            this.titulo = titulo;
+            this.autor = autor;
+            this.anio = anio;
+        }
+
+        crearLibro(tit, aut, an){
+            this.titulo = tit;
+            this.autor = aut;
+            this.anio = an;
+        }
+
+    }
 
 
+    $btnCrear.addEventListener("click", () => {
+
+        const $titulo = document.querySelector(titulo).value;
+        const $autor = document.querySelector(autor).value;
+        const $anio = document.querySelector(anio).value;
     
+
+        
+
+    });
+
+
+
 }
 
 
