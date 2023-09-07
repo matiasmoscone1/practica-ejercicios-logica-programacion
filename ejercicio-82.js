1fd4627ca74de6bc7727dc6023c4410e41e8ab51
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-07 19:39:02 -0300

    Resolviendo logica de objetos y arrays ejercicio 82

--- ejercicio-82.js
+++ ejercicio-82.js
@@ -6,20 +6,32 @@
 
 export default function tiendaOnline(nombre, precio, btnAgregar, lista, btnCalcular, resultado, btnLimpiar){
 
+    const $btnAgregar = document.querySelector(btnAgregar);
     const $btnCalcular = document.querySelector(btnCalcular);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
-    
+
     class TiendaOnline{
-        constructor(carrito){
-            this.carrito = carrito;
+        constructor(){
+            this.carrito = [];
         }
     }
-
+    /*
     agregarCarrito(producto){
+        this.carrito = 
+    }*/
 
-    }
+    const instanciaTienda = new TiendaOnline;
 
+    $btnAgregar.addEventListener("click", () => {
+        const $nombre = document.querySelector(nombre).value;
+        const $precio = document.querySelector(precio).value;
+    
+        let nuevoProducto = {nombre: $nombre, precio: $precio};
+
+        instanciaTienda.agregarCarrito(nuevoProducto);
+    
+    });
 
 
 
