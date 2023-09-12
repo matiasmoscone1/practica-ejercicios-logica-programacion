0c2c3239e6e531c12c683c3398ea93033e04682c
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-12 19:45:17 -0300

    Resolviendo logica del ejercicio 85

--- ejercicio-85.js
+++ ejercicio-85.js
@@ -5,7 +5,28 @@
 export default function ejercicioPlaylist(playlist, btnReproducir, btnParar, resultado, btnAgregarUno, btnAgregarDos, btnAgregarTres){
 
 
-    
+
+    class Playlist{
+        constructor(){
+            this.canciones = [];
+        }
+
+        agregarCancion(cancion){
+            this.canciones.push(cancion);
+        }
+
+    }
+
+    const instanciaPlaylist = new Playlist();
+
+    document.addEventListener("click", (e) => {
+        if(e.target.matches(btnAgregarUno)){
+            instanciaPlaylist.agregarCancion("Cancion 1");
+        }
+
+        console.log(instanciaPlaylist.canciones);
+    });
+
 
 
 
