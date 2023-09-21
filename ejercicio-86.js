72c4c078e2e29fb2bedcf83936ec706d50776046
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-21 20:00:09 -0300

    Resolviendo ej 86

--- ejercicio-86.js
+++ ejercicio-86.js
@@ -6,6 +6,29 @@
 export default function calculaVehiculo(marca, modelo, color, btnCalcular, btnLimpiar, resultado){
 
 
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
+
+
+    class Vehiculo{
+        constructor(marca, modelo, color){
+            this.marca = marca;
+            this.modelo = modelo;
+            this.color = color;
+        }
+
+
+
+
+
+    }
+
+
+
+    $btnCalcular.addEventListener("click", () => {
+        
+    });
 
 
 
