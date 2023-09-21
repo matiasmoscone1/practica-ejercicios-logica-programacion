f05fa5f96d73fbbd007bca1140efd55b80ae8005
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-21 20:16:14 -0300

    Agregando ejercicio 87

---
+++ ejercicio-87.js
@@ -0,0 +1,15 @@
+
+
+//87. Crea una promesa que simule una llamada a una API y maneja tanto el caso de 
+//éxito como el caso de error. (API JSON)
+
+
+export default function datosUsuarios(){
+
+
+
+    
+
+
+}
+
--- index.js
+++ index.js
@@ -84,6 +84,7 @@ import muestraScroll from "./ejercicio-83.js";
 import estudiantes from "./ejercicio-84.js";
 import ejercicioPlaylist from "./ejercicio-85.js";
 import calculaVehiculo from "./ejercicio-86.js";
+import datosUsuarios from "./ejercicio-87.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -174,6 +175,7 @@ document.addEventListener("DOMContentLoaded", () => {
     estudiantes(".btn-agregar-ej-84", ".btn-limpiar-ej-84", ".lista-alumnos-ej-84", ".btn-calcular-ej-84",".resultado-ej-84");
     ejercicioPlaylist(".div-playlist-ej-85", ".btn-reproducir-ej-85", ".btn-parar-ej-85", ".resultado-reproducir-ej-85", ".btn-agregar-uno-ej-85", ".btn-agregar-dos-ej-85", ".btn-agregar-tres-ej-85")
     calculaVehiculo(".input-marca-ej-86", ".input-modelo-ej-86", ".input-color-ej-86", ".btn-calcular-ej-86", ".btn-limpiar-ej-86", ".resultado-ej-86");
-
+    datosUsuarios();
+    
 
 });
