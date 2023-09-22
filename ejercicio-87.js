689242651716389d8e1269561709fe84530695ce
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-09-22 20:17:51 -0300

    Ejercicio 87 documentado completo

--- ejercicio-87.js
+++ ejercicio-87.js
@@ -7,20 +7,29 @@
 
 export default function datosUsuarios(btnIniciar, btnParar, resultado){
 
+    //creando variable URL que almacenara la url de la api
     const URL = "https://jsonplaceholder.org/users";
 
+    //creando variables referenciales al DOM
     const $btnIniciar = document.querySelector(btnIniciar);
     const $btnParar = document.querySelector(btnParar);
     const $resultado = document.querySelector(resultado);
 
+    //Creando array vacio donde se almacenaran todos los datos de la API
     let datos = [];
 
+    //Creando funcion llamadora de la API, Es una funcion asincrona
     const callAPI = async () => {
         
+        //bloque try catch donde intenta hacer el llamado a la api mediante metodo fetch
         try{            
+            //se le agrega await con el fin de esperar a que la promesa se ejecute antes de 
+            //continuar el codigo 
             await fetch(URL)
             .then((response) => response.json())
+            //clona el array de datos de la api al array vacio creado anteriormente
             .then((data) => {datos = [...data]});
+            //manipula el error
         }catch(e){
             console.log({e});
         }
@@ -28,17 +37,21 @@ export default function datosUsuarios(btnIniciar, btnParar, resultado){
 
     };
 
-
+    //captando evento click del boton Iniciar, se pasa una callback asincrona
     $btnIniciar.addEventListener("click", async () => {
         
+        //se le agrega await con el fin de esperar a que se ejecute la funcion para no bloquear
+        //el codigo
         await callAPI();
         //console.log(datos);
 
+        //Muestra todo el resultado de los usuarios en el DOM
         $resultado.innerHTML = `${datos.map((usuario) => `Nombre: ${usuario.firstname} - Apellido: ${usuario.lastname} - Email: ${usuario.email} <br>`).join("")}`;
     });
 
-
+    //captando el evento click del boton Limpiar
     $btnParar.addEventListener("click", () => {
+        //limpia el resultado en el DOM
         $resultado.innerHTML = "";
     });
 
