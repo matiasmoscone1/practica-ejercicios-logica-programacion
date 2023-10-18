c5329d027337ffbaba1de940b8a9913131a2b27f
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-18 17:05:34 -0300

    Documentando ejercicio 89

--- ejercicio-89.js
+++ ejercicio-89.js
@@ -4,16 +4,21 @@
 
 export default function dobleNumerosPares(matriz, btnFiltrar, btnLimpiar, resultado){
 
+    //creando variables referenciales al DOM
     const $matriz = document.querySelector(matriz);
     const $btnFiltrar = document.querySelector(btnFiltrar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //creando matriz principal de numeros
     const arrayNumeros = [2, 64, 33, 16, 25, 43, 11, 22, 14, 19];
 
+    //mostrando matriz principal en el DOM
     $matriz.textContent = `[${arrayNumeros}]`;
 
+    //creando funcion que recibe por parametro una matriz de numeros
     const filtrarMatriz = (mat) => {
+        //devuelve el array filtrado con los numeros pares y multiplicados por dos
         return mat.filter((valor) => ((valor % 2) === 0)).map((val) => val * 2);
     }
 
