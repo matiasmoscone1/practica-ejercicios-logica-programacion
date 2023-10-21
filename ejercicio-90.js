f1d350ae5557feda5e81600d2311ea2a94298c21
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-21 16:35:51 -0300

    Agregando ejercicio 90

---
+++ ejercicio-90.js
@@ -0,0 +1,14 @@
+
+//90. Suma de todos los números: Escribe una función que tome una matriz de números y 
+//use reduce para sumar todos los elementos.
+
+
+export default function sumarNumerosPares(){
+
+    
+
+
+
+}
+
+
