89f4dce0406d29a6de7262e7a41a6487ac311589
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-21 18:45:30 -0300

    Resolviendo ejercicio 91

--- ejercicio-91.js
+++ ejercicio-91.js
@@ -2,10 +2,19 @@
 //91. Factorial recursivo: Implementa una función recursiva para 
 //calcular el factorial de un número.
 
+//factorial se define como el producto de todos los num anteriores o iguales a el 
 export default function factorialRecursivo(inputNum, btnCalcular, btnLimpiar, resultado){
 
+
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
     
-    
+    $btnCalcular.addEventListener("click", () => {
+        const $inputNum = document.querySelector(inputNum);
+
+        
+    });
 
 
 }
