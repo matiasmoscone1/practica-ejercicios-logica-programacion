14b8e9d8719ad306699aee51fb80f3100197ccb7
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-26 16:25:43 -0300

    Resolviendo ejercicio 93

--- ejercicio-93.js
+++ ejercicio-93.js
@@ -8,6 +8,20 @@ export default function currying(inputNum, btnCalcular, btnLimpiar, resultado){
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    const curry = (func) => {
+        return curried = (...argumentos) => {
+            if(argumentos.length >= func.length){
+                return func(...argumentos);
+            }else{
+                return (...masArgumentos) => curried(...argumentos, ...masArgumentos);
+            }
+            
+        }
+    }
+
+    const suma = (a, b, c) => {
+        return a + b + c;
+    }
 
 
     $btnCalcular.addEventListener("click", () => {
