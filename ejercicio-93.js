72968e7d46cf66e32e85ac56e923edb0c3132690
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-26 16:08:47 -0300

    Agregando ejercicio 93

---
+++ ejercicio-93.js
@@ -0,0 +1,13 @@
+
+//93. Currying: Implementa una función que convierta una función de varios argumentos 
+//en una secuencia de funciones que toman un solo argumento.
+
+export default function currying(){
+
+
+
+    
+}
+
+
+
