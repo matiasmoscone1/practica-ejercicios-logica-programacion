52d8f7303af0abfad31a019aced6bca047161f19
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-26 16:20:11 -0300

    Resolviendo ejercicio 93

--- ejercicio-93.js
+++ ejercicio-93.js
@@ -4,6 +4,17 @@
 
 export default function currying(inputNum, btnCalcular, btnLimpiar, resultado){
 
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
+
+
+
+    $btnCalcular.addEventListener("click", () => {
+
+
+
+    });
 
 
 
