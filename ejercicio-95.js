56783bc3d095c4238d6f747271378e901506100a
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-10-29 18:59:05 -0300

    Resolviendo ejercicio 95

--- ejercicio-95.js
+++ ejercicio-95.js
@@ -3,8 +3,15 @@
 //95. Map recursivo: Escribe una función que aplique una función dada a cada elemento de un array y 
 //sus elementos anidados de manera recursiva.
 
-export default function mapRecursivo(){
+export default function mapRecursivo(arrayPrincipal, btnFuncion, btnLimpiar, resultado){
 
+    const $arrayPrincipal = document.querySelector(arrayPrincipal);
+    const $btnFuncion = document.querySelector(btnFuncion);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
+
+
+    
 
 
 
