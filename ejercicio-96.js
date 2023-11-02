8e796f4f4601fbb08bd139a59d98905496c069e9
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-02 16:56:50 -0300

    Documentando ejercicio 96

--- ejercicio-96.js
+++ ejercicio-96.js
@@ -5,41 +5,53 @@
 
 export default function arbolRecursivo(arbol, btnCalcular, btnLimpiar, resultado){
 
+    //creando estructura arbol
     class Arbol{
+        //creando el constructor del arbol
         constructor(valor){
+            //añadiendo propiedades del arbol
             this.valor = valor;
             this.hijo = [];
         }
     
+        //añadiendo funcionalidad, se le pasa un nodo hijo a un nodo padre
         agregarHijo(nodoHijo){
             this.hijo.push(nodoHijo);
         }
     }
 
+    //crea tres nodos distintos y uno es el principal
     const nodoPrincipal = new Arbol("A");
     const nodoB = new Arbol("B");
     const nodoC = new Arbol("C");
 
+    //al nodo principal se le agregan como hijos B y C
     nodoPrincipal.agregarHijo(nodoB);
     nodoPrincipal.agregarHijo(nodoC);
 
+    //se crean dos nodos mas D y E
     const nodoD = new Arbol("D");
     const nodoE = new Arbol("E");
 
+    //al nodo B se le agregan como hijos D y E
     nodoB.agregarHijo(nodoD);
     nodoB.agregarHijo(nodoE);
 
+    //se crea un nodo F
     const nodoF = new Arbol("F");
 
+    //al nodo C se le agrega como hijo el nodo F
     nodoC.agregarHijo(nodoF);
 
-    console.log(nodoPrincipal);
+    //console.log(nodoPrincipal);
 
+    //se crean variables referenciales al DOM
     const $arbol = document.querySelector(arbol);
     const $btnCalcular = document.querySelector(btnCalcular);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //se crea un array de nodos vacio
     const arrayNodos = [];
   
     const mostrarNodo = (nodo) => {
