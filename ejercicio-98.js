699abf1f97b2682314b6870bf0affa43359b53a0
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-05 13:53:26 -0300

    Documentando ejercicio 98

--- ejercicio-98.js
+++ ejercicio-98.js
@@ -7,18 +7,24 @@
 export default function devolverPromesa(input, btnComenzar, btnLimpiar, resultado){
 
 
+    //creando variables referenciales al DOM
     const $btnComenzar = document.querySelector(btnComenzar);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
 
+    //creando promesa, se le pasa un texto como parametro
     const creaPromesa = (texto) => {
+        //retorna la nueva promesa, pasandole por parametro res que significa resolucion
         return new Promise((res) => {
+            //despues de los 2 segundos resuelve la promesa
             setTimeout(() => {
+                //se resuelve la promesa, mostrando en pantalla el texto pasado por parametro
                 res($resultado.textContent = `El resultado es: ${texto}`);
+            //se agregan 2000 milisegundos, que son 2 segundos para el timeout
             }, 2000);
         });
-        }
+    }
 
 
     $btnComenzar.addEventListener("click", async () => {
