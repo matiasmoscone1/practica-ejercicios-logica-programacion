a9914bac110e631395707e4cc34cfb54261a555a
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-06 16:22:11 -0300

    Ejercicio 98 documentado completamente

--- ejercicio-98.js
+++ ejercicio-98.js
@@ -26,17 +26,23 @@ export default function devolverPromesa(input, btnComenzar, btnLimpiar, resultad
         });
     }
 
-
+    //capta evento click del boton Comenzar y se le asigna una callback asincrona
     $btnComenzar.addEventListener("click", async () => {
+        //toma el valor del input escrito por el usuario
         const $inputText = document.querySelector(input).value;
 
+        //la funcion asincrona anterior no se ejecutara hasta que esta funcion haya sido
+        //ejecutada correctamente, se le pasa el valor del input por parametro
         await creaPromesa($inputText)
+        //luego si todo sale bien, la promesa devuelve el resultado correctamente
             .then((resultado) => $resultado.textContent = resultado)
+        //sino muestra el error en el DOM
             .catch((err) => $resultado.textContent = `Hubo un error: ${err}`);
-    
     });
 
+    //capta evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //limpia el resultado en el DOM y el input 
         $resultado.textContent = "";
         document.querySelector(input).value = "";
     });
