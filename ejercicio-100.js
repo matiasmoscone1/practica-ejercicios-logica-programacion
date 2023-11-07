ed26d6502e7aa2c580a4dfacf9bff072151c4730
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-07 16:29:51 -0300

    Agregando ejercicio 100

---
+++ ejercicio-100.js
@@ -0,0 +1,15 @@
+
+//100. Gestión de errores con promesas: Crea una función que simule una llamada a una 
+//API que puede arrojar errores. Utiliza promesas y try/catch para manejar los 
+//errores de manera adecuada.
+
+export default function gestionErrores(){
+
+
+
+
+
+
+    
+}
+
