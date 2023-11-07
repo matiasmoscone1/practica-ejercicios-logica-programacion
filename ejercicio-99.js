32fe6c1e98d53c8c4e52bae47ee2c4b1cce919c7
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-07 16:24:40 -0300

    Documentando ejercicio 99

--- ejercicio-99.js
+++ ejercicio-99.js
@@ -28,36 +28,52 @@ export default function generadorPrimos(numeros, input, btnGenerar, btnFiltrar, 
         return acc;
     }
 
+    //funcion que determina si un numero es primo o no
     const esPrimo = (num) => {
+        //si es 0 o 1, no es primo
         if(num === 0 || num === 1){
             return false;
         }
         //se utiliza i * i para optimizar el calculo de si un nro es primo o no
         for(let i = 2; i * i <= num; i++){
+            //divide el numero sobre el iterador y verifica si el resto da 0, si da 0, no es primo
             if(num % i === 0){
                 return false;
             }
         }
+        //retorna el numero primo
         return true;
     }
 
+    //filtra los numeros primos, se le pasa por parametro un array de numeros
     const filtraPrimos = (arr) => {
+        //utiliza reduce para filtrar
         const arrayReducido = arr.reduce((acc, val) => {
+            //a cada numero del array lo filtra si es primo o no llamando a la funcion
             if(esPrimo(val)){
+                //si es primo, se agrega al array acc
                 acc.push(val);
             }
+            //devuelve el array con numeros primos
             return acc;
+            //se inicializa acc como array vacio
         }, []);
+        //devuelve el array reducido
         return arrayReducido;
     }
     
+    //capta el evento click del boton Generar
     $btnGenerar.addEventListener("click", () => {        
+        //toma el valor del input escrito por el usuario
         const $inputNum = document.querySelector(input).value;
+        //constante que llama a la funcion generadora de numeros
         const res = generaNumeros($inputNum);
         //console.log(res);
 
+        //muestra los numeros generados en pantalla
         $numeros.textContent = `${res.join(", ")}`;
 
+        
         $btnFiltrar.addEventListener("click", () => {
             const respuesta = filtraPrimos(res);
             $resultado.textContent = `El resultado es: ${respuesta.join(", ")}`;
