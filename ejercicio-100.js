711981082313b103680ab9f4512114be28d594a9
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-08 17:59:18 -0300

    Resolviendo logica de llamada a funcion para extraer datos de la api

--- ejercicio-100.js
+++ ejercicio-100.js
@@ -12,12 +12,12 @@ export default function gestionErrores(btnLlamar, btnLimpiar, resultado, error){
     const $error = document.querySelector(error);
  
     const URL = "https://jsonplaceholder.org/users";
-    const datos = "";
+    let datos = {};
 
     const callAPI = () => {
         fetch(URL)
         .then((response) => response.json())
-        .then((data) => datos = data)
+        .then((data) => datos = {...data})
         
     }
 
