a8b8fa2fa4849f5d52be8f10996aedbfb8a0d0c6
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-09 17:11:06 -0300

    Agregando ejercicio 101

---
+++ ejercicio-101.js
@@ -0,0 +1,13 @@
+
+//101. Crea una función que tome una matriz de números y devuelva otra matriz con los 
+//números ordenados de menor a mayor, sin usar el método sort.
+
+
+export default function matrizOrdenada(){
+
+
+
+    
+}
+
+
