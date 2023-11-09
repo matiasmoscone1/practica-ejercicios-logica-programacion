ac1e49cb1174083059f71fee12cca60f0f2eb1dd
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-09 17:02:39 -0300

    Documentando ejercicio 100

--- ejercicio-100.js
+++ ejercicio-100.js
@@ -5,26 +5,34 @@
 
 export default function gestionErrores(btnLlamar, btnError, btnLimpiar, resultado, error){
 
-
+    //creando variables referenciales al DOM
     const $btnLlamar = document.querySelector(btnLlamar);
     const $btnError = document.querySelector(btnError);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
     const $error = document.querySelector(error);
  
+    //creando constantes de URL de prueba y arreglo vacio
     const URL = "https://jsonplaceholder.org/users";
     const URLError = "https://jsonplaceholder.org/uses";
     let datos = [];
 
+    //funcion llamadora de api, recibe por parametro la url a resolver, es una funcion asincrona
     const callAPI = async(url) => {
+        //bloque try que intenta resolver la promesa fetch para obtener datos de una api
         try{    
+            //fetch con await que una vez que este lista la peticion fetch, termina de ejecutar callAPI
             await fetch(url)
+            //guarda la respuesta en formato json
             .then((response) => response.json())
+            //mete los datos de la peticion en el arreglo vacio previamente creado
             .then((data) => datos.push(data))
+            //llama a la funcion mostrar datos para mostrar datos en DOM
             mostrarDatos();
+            //bloque catch que agarra el error
         }catch(err){
             //console.log(`Error al obtener los datos ${err}`);
-            console.log(err);
+            //muestra el error en el DOM
             $error.textContent = `Error: "${err}"`;
         }
     }
