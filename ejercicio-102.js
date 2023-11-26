6c24bb7f9a1ba43e0fc0aa9e48faa4e54dd512e4
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-11-26 16:37:06 -0300

    Documentando ejercicio 102

--- ejercicio-102.js
+++ ejercicio-102.js
@@ -6,20 +6,21 @@
 
 export default function informacionNavegador(btnVer, btnLimpiar, resultado){
 
+    //creando variables referenciales al DOM
     const $btnVer = document.querySelector(btnVer);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //captando evento click del boton Ver
     $btnVer.addEventListener("click", () => {
-
-        console.log(navigator);
-        
+        //console.log(navigator);
+        //llamando al objeto navigator y accediendo a sus propiedades, mostrando en el DOM el resultado
         $resultado.innerHTML = `Nombre: ${navigator.appName} <br><br> Version: ${navigator.appVersion} <br><br> Idioma: ${navigator.language}`;
-
     });
 
-    
+    //captando evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //limpia el resultado en el DOM
         $resultado.textContent = "";
     });
     
