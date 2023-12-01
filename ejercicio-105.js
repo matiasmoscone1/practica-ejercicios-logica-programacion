0a6c634dc482253d0ad4b90ff9b2bbfe0f0054f3
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-01 17:09:47 -0300

    Ejercicio 105 documentado con exito

--- ejercicio-105.js
+++ ejercicio-105.js
@@ -6,33 +6,45 @@
 
 export default function contadorVisitas(resultado, btnLimpiar){
 
+    //creando variables referenciales al DOM
     const $resultado = document.querySelector(resultado);
     const $btnLimpiar = document.querySelector(btnLimpiar);
 
+    //creando contador inicial
     let count = 0;
     
     //localStorage.clear();
 
 
+    //escuchando el evento beforeunload (antes de recargar), del objeto window
     window.addEventListener("beforeunload", () => {
 
+        //obteniendo en la variable contador, el "Contador" que ya tiene almacenado el localStorage
+        //y sumandole 1, sino tiene nada el localStorage, se le suma 1 igual y queda con valor 1
         const contador = (parseInt(localStorage.getItem("Contador")) + 1)  || (parseInt(count) + 1);
 
+        //guarda en localStorage el "Contador" y se le asigna la variable contador declarada recien
         localStorage.setItem("Contador", contador);
 
         
         //console.log("SE RECARGO 1 VEZ");
     });
 
+
+    //crea nuevamente una variable contador pero para obtener la informacion del localStorage
     const contador = localStorage.getItem("Contador");
 
+    //muestra el resultado en el DOM
     $resultado.textContent = `Visitas: ${contador}`;
 
 
     //console.log(contador);
 
+    //capta el evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //limpia el localStorage
         localStorage.clear();
+        //recarga la pagina
         location.reload();
     })
 
