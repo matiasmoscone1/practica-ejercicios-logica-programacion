716c845fa750c5f86056f963cbf845c915d7f70b
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-02 17:55:40 -0300

    Ejercicio 106 terminado con exito, faltan detalles

--- ejercicio-106.js
+++ ejercicio-106.js
@@ -8,20 +8,44 @@ export default function cambiarFondo(btnOscuro, btnClaro){
     const $btnOscuro = document.querySelector(btnOscuro);
     const $btnClaro = document.querySelector(btnClaro);
 
-    const colorFondo = localStorage.setItem("color", "white");
+    let flag = true;
+
+
 
     console.log(localStorage.getItem("color"));
 
+    if(flag){
+        localStorage.setItem("color", "white");
+    }else{
+        localStorage.setItem("color", "black");
+    }
 
     $btnOscuro.addEventListener("click", () => {
-        document.body.style.backgroundColor = "#000000";
-        document.body.style.color = "#FFFFFF";
+        
+    
+        const colorFondo = localStorage.getItem("color");
+
+        if(colorFondo === "white"){
+            document.body.style.backgroundColor = "#000000";
+            document.body.style.color = "#FFFFFF";
+            localStorage.setItem("color", "black");
+        }else{
+            document.body.style.backgroundColor = "#FFFFFF";
+            document.body.style.color = "#000000";
+            localStorage.setItem("color", "white");     
+        }
+        
+    
+    
     });
 
+    /*
     $btnClaro.addEventListener("click", () => {
         document.body.style.backgroundColor = "#FFFFFF";
         document.body.style.color = "#000000";     
-    });
+    });*/
+
+
 
 
 
