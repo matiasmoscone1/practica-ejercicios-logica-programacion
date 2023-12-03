d951fe91094b0f073d01cc94274787a621bbd647
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-03 15:21:06 -0300

    Ejercicio 107 documentado completo con exito  y mejorado un intervalo de horario

--- ejercicio-107.js
+++ ejercicio-107.js
@@ -9,23 +9,28 @@
 
 export default function cambiaFondoPorHora(btnIniciar, btnFinalizar){
 
-    //
+    //creando variables referenciales al DOM
     const $btnIniciar = document.querySelector(btnIniciar);
     const $btnFinalizar = document.querySelector(btnFinalizar);
 
+    //creando variable hora que contendra el objeto Date con la fecha actual
     const hora = new Date;
-    
+
+    //captando evento click del boton Iniciar
     $btnIniciar.addEventListener("click", () => {
 
         console.log(hora.getHours());
 
+        //Logica de intervalo de horas, si esta entre las 6 y las 12, es fondo blanco
+        //si esta entre las 12 y las 18, es fondo gris
+        //si esta entre las 18 y las 6 es fondo negro
         if(hora.getHours() > 6 && hora.getHours() < 12){
             document.body.style.backgroundColor = "#FFFFFF";
             document.body.style.color = "#000000";
         }else if(hora.getHours() > 12 && hora.getHours() < 18){
             document.body.style.backgroundColor = "#808080";
             document.body.style.color = "#FFFFFF";    
-        }else if(hora > 18 && hora < 6){
+        }else if((hora > 18 && hora < 23) || hora < 6){
             document.body.style.backgroundColor = "#000000";
             document.body.style.color = "#FFFFFF";
         }
