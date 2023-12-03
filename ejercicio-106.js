a6d8c375a7e9c61688d09736750c69369b054818
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-03 14:27:40 -0300

    Documentando ejercicio 106

--- ejercicio-106.js
+++ ejercicio-106.js
@@ -8,22 +8,18 @@ export default function cambiarFondo(btnFondo){
     //Creando variables referenciales al DOM
     const $btnFondo = document.querySelector(btnFondo);
 
-    //console.log(localStorage.getItem("color"));
-
-
-    //seteando primer color en el localstorage
-    localStorage.setItem("color", "white");
-
-
-    
+    //captando evento click del boton Fondo
     $btnFondo.addEventListener("click", () => {
     
-        const colorFondo = localStorage.getItem("color");
+        //obteniendo el color del localStorage o si no hay, setear el color white como primera instancia
+        const colorFondo = localStorage.getItem("color") || localStorage.setItem("color", "white");
 
+        //si la variable color es "white", se cambia todo el fondo a black, y setea el localStorage en black
         if(colorFondo === "white"){
             document.body.style.backgroundColor = "#000000";
             document.body.style.color = "#FFFFFF";
             localStorage.setItem("color", "black");
+        //si no es "white", se cambia todo el fondo a white y setea el localStorage en white
         }else{
             document.body.style.backgroundColor = "#FFFFFF";
             document.body.style.color = "#000000";
