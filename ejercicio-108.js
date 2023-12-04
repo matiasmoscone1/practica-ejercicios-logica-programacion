5a88e57bd5e0720d9c149991ab07e0f1dde19082
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-04 15:05:53 -0300

    Ejercicio 108 terminado con exito

--- ejercicio-108.js
+++ ejercicio-108.js
@@ -26,7 +26,9 @@ export default function validacionGeolocalizacion(btnCoords, btnLimpiar, resulta
         });
     });
 
-
+    $btnLimpiar.addEventListener("click", () => {
+        $resultado.textContent = "";
+    })
 
 
 
