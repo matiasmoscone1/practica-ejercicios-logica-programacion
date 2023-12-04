24fddb4ad9747ffda1c94cd3fe0fccff79ae5c93
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-04 15:10:19 -0300

    Documentando ejercicio 108 con exito

--- ejercicio-108.js
+++ ejercicio-108.js
@@ -12,25 +12,34 @@ export default function validacionGeolocalizacion(btnCoords, btnLimpiar, resulta
 
     //console.log(locacion);
 
+    //creando variables referenciales al DOM
     const $btnCoords = document.querySelector(btnCoords);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    //captando evento click del boton Ver Coords
     $btnCoords.addEventListener("click", () => {
-        
+
+        //haciendo una validacion si el navegador es compatible con el objeto geolocalizacion        
         if(navigator.geolocation){        
+            //que de ese objeto extraiga la posicion actual del usuario
             navigator.geolocation.getCurrentPosition((position) => {
+                //guardando latitud y longitud en variables para mostrarlas despues por el DOM
                 const latitud = position.coords.latitude;
                 const longitud = position.coords.longitude;
-                //console.log(latitud,longitud);
+                //Mostrando resultado por el DOM
                 $resultado.innerHTML = `<p>Latitud: ${latitud}</p><p>Longitud: ${longitud}</p>`
             });
+        //si el navegador no es compatible con el objeto geolocalizacion, muestra un mensaje en
+        //el DOM
         }else{
             $resultado.textContent = "La Geolocalizacion no es compatible con este navegador...";
         }
     });
 
+    //captando evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
+        //limpia el DOM
         $resultado.textContent = "";
     })
 
