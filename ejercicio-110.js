41a670ce3a060ac17d6ba77886efcace706a1998
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-06 15:15:56 -0300

    Maquetando ejercicio 110

--- ejercicio-110.js
+++ ejercicio-110.js
@@ -1,9 +1,16 @@
 
 //110. Geolocation API: Implementa una función que calcule la distancia entre 
-//la ubicacion del usuario y otros puntos de referencia
+//la ubicacion del usuario y otros puntos de referencia (3 puntos de referencia de Córdoba)
 
 export default function distanciaGeolocalizacion(ubicacion, btnCalcular, btnLimpiar, resultado){
 
+    const $ubicacion = document.querySelector(ubicacion);
+    const $btnCalcular = document.querySelector(btnCalcular);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
+
+
+    
 
 
 
