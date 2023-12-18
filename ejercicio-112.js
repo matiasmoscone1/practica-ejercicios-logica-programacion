f2ba39fe15b12e96e92a8d3fe0609780a18dd49a
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-18 17:10:14 -0300

    Agregando ejercicio 112

---
+++ ejercicio-112.js
@@ -0,0 +1,18 @@
+
+
+//112. Location Object:
+//a. Crea una página web con enlaces internos y usa el objeto Location
+//para mostrar la URL de la página actual.
+//b. Implementa una función que redirija a los usuarios a una URL diferente
+//después de cierto tiempo.
+
+
+export default function locationObject(){
+
+
+
+
+
+}
+
+
