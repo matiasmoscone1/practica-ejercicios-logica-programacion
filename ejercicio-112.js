d9f3dd3ac9d7ccc9a51426775575d8b32f48b562
matiasmoscone1 <matiasmoscone1@gmail.com>
2023-12-20 17:24:38 -0300

    Resolviendo porque no trae la url de la ventana emergente del ej 112

--- ejercicio-112.js
+++ ejercicio-112.js
@@ -15,8 +15,10 @@ export default function locationObject(btnFacebook, btnInstagram, btnGmail, btnY
         let ventana;
 
         if(e.target.matches(btnFacebook)){
-            ventana = window.open('https://www.facebook.com/', "Facebook", "_blank", 'width=600,height=600');
-            console.log(ventana.name);
+            ventana = window.open(async () => {
+                return 'https://www.facebook.com/';
+            }, "Facebook", "_blank", 'width=600,height=600');
+            console.log(ventana.location.href);
         }else if(e.target.matches(btnInstagram)){
             window.open('https://www.instagram.com/', "_blank", 'width=600,height=600');
         }else if(e.target.matches(btnGmail)){
