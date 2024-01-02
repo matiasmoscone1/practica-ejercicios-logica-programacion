402b8ba56a3125bcbe5847c3fc7f07217df0a0fb
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-02 17:03:09 -0300

    Documentando ejercicio 115

--- ejercicio-115.js
+++ ejercicio-115.js
@@ -8,37 +8,44 @@
 
 export default function devsLeagueReto(input, btnAgregar, btnLimpiar ,almacenamiento, resultado){
 
+    //creando variables referenciales al DOM
     const $btnAgregar = document.querySelector(btnAgregar);
-    
     const $btnLimpiar = document.querySelector(btnLimpiar);
-    
     const $almacenamiento = document.querySelector(almacenamiento);
     const $resutlado = document.querySelector(resultado);
 
+    //creando array que almacenara todo el input del usuario y cadena vacia
     const stack = [];
     let cadena = "";
+    //creando expresion regular para validar el input
     const validaInput = /^(\(\)|\{\}|\[\]|\(\[\{\}\]\))*$/;
 
+    //mostrando en el DOM el valor de la cadena
     $almacenamiento.textContent = `Cadena: ${cadena}`;
 
+    //captando evento click del boton Agregar
     $btnAgregar.addEventListener("click", () => {
+        //tomando el valor del input del usuario
         const $caracter = document.querySelector(input).value;
 
-
+        //metiendo en variable stack todo lo del input
         stack.push($caracter);
 
+        //convirtiendo el array en una cadena para poder validarlo
         cadena = stack.join("");
     
+        //Si valida con exito la condicion, muestra en el DOM el resultado esperado
         if(validaInput.test(cadena)){
-            console.log("Input validado con exito!!!");
+            //console.log("Input validado con exito!!!");
             $resutlado.textContent = "Input validado con exito!!!";
+            //sino, muestra que es invalido el input
         }else{
-            console.log("Input invalido...");
+            //console.log("Input invalido...");
             $resutlado.textContent = "Input invalido...";
         }
 
 
-        console.log(cadena);
+        //console.log(cadena);
 
         $almacenamiento.textContent = `Cadena: ${cadena}`;
 
