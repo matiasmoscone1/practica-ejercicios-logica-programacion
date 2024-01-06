4d2c57d35873a598b4347a726f052d9295100ffa
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-06 17:26:50 -0300

    Resolviendo ejercicio 117

--- ejercicio-117.js
+++ ejercicio-117.js
@@ -7,7 +7,19 @@
 export default function buscaSubCadena(textarea, input, btnBuscar, btnLimpiar, resultado){
 
 
+    const $btnBuscar = document.querySelector(btnBuscar);
+    const $btnLimpiar = document.querySelector(btnLimpiar);
+    const $resultado = document.querySelector(resultado);
+    
 
+    $btnBuscar.addEventListener("click", () => {
+        const $textArea = document.querySelector(textarea).value;
+        const $inputSubCadena = document.querySelector(input).value;
+
+        
+
+
+    });
 
 
 
