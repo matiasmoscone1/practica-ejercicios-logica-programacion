2d10e0183a81f3b22253ec28d5fd5b44be69100b
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-13 00:07:47 -0300

    Documentando ejercicio 118

--- ejercicio-118.js
+++ ejercicio-118.js
@@ -6,35 +6,45 @@
 
 export default function recorrerGrafoDFS(btnMostrar, grafo, nodoInicial ,btnRecoorer, btnLimpiar){
 
+    //creando variables referenciales al DOM
     const $btnMostrar = document.querySelector(btnMostrar);
     const $grafo = document.querySelector(grafo);
     const $btnRecorrer = document.querySelector(btnRecoorer);
     const $btnLimpiar = document.querySelector(btnLimpiar);
 
-
+    //creando grafo
     class Grafo {
+        //constructor
         constructor(){
             this.nodos = {};
         }
         
+        //funcion/metodo que agrega un nodo
         agregarNodo(valor){
             this.nodos[valor] = [];
         }
 
+        //agrega la arista de un nodo al otro
         agregarArista(nodo1, nodo2) {
             this.nodos[nodo1].push(nodo2);
             this.nodos[nodo2].push(nodo1);
           }
         
+      //imprime el grafo en el DOM
         imprimirGrafo() {
+            //crea una variable vacia de grafoCompleto
             let grafoCompleto = "";
+            //recorre el grafo y crea los vecinos de cada nodo
             for (const nodo in this.nodos) {
                 const vecinos = this.nodos[nodo].join(', ');
+                //almacena en la variable vacia el formato en q se representa el nodo y sus vecinos
                 grafoCompleto += `${nodo} -> [${vecinos}] <br>`;
                 //console.log(`${nodo} -> [${vecinos}]`);
             }
+            //muestra en el DOM el resultado
             $grafo.innerHTML = grafoCompleto;
         }
+        
         algoritmoDFS(nodo, nodosVisitados){
 
             nodosVisitados.add(nodo);
