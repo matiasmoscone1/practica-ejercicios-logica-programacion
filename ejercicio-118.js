c782716f649f4f2026f62e5c5bf86edb9f0c4815
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-13 00:16:47 -0300

    ejercicio 118 documentado completo

--- ejercicio-118.js
+++ ejercicio-118.js
@@ -60,9 +60,13 @@ export default function recorrerGrafoDFS(btnMostrar, grafo, nodoInicial ,btnReco
             //console.log(`Visitando el nodo: ${nodo}`);
             mostrarRecorrido += `Visitando el nodo: ${nodo} <br>`;
 
+            //muestra el resultado del recorrido en el DOM
             $resultado.innerHTML = mostrarRecorrido;
 
+            //recorre el nodo y los vecinos
             for(const vecino of this.nodos[nodo]){
+                //si no existe el vecino en el Set de nodosVisitados, se hace recursion y se
+                //llama nuevamente a la funcion algoritmoDFS para que siga recorriendo todos los nodos
                 if(!nodosVisitados.has(vecino)){
                     this.algoritmoDFS(vecino, nodosVisitados);
                 }
@@ -72,58 +76,53 @@ export default function recorrerGrafoDFS(btnMostrar, grafo, nodoInicial ,btnReco
         
     }
 
+    //captando evento click del boton Mostrar
     $btnMostrar.addEventListener("click", () => {
 
+        //crea el Grafo
         const miGrafo = new Grafo;
         
+        //se le agregan todos los nodos
         miGrafo.agregarNodo('A');
         miGrafo.agregarNodo('B');
         miGrafo.agregarNodo('C');
         miGrafo.agregarNodo('D');
         miGrafo.agregarNodo('E');
     
+        //se le agregan todas las aristas a los nodos
         miGrafo.agregarArista('A', 'B');
         miGrafo.agregarArista('B', 'C');
         miGrafo.agregarArista('C', 'D');
         miGrafo.agregarArista('D', 'A');
         miGrafo.agregarArista('D', 'E');
     
-    
+        //imprime el grafo en el DOM
         miGrafo.imprimirGrafo();
     })
 
-
-/*
-    const algoritmoDFS = (nodo, nodosVisitados) => {
-
-        nodosVisitados.add(nodo);
-        console.log(`Visitando el nodo: ${nodo}`);
-
-        for(const vecino of this.nodos[nodo]){
-            if(!nodosVisitados.has(vecino)){
-                this.algoritmoDFS(vecino, nodosVisitados);
-            }
-        }
-
-    }*/
-
+    //captando click del boton Recorrer
     $btnRecorrer.addEventListener("click", () => {
+        //tomando el valor del input del usuario del nodo inicial
         const $nodoInicial = document.querySelector(nodoInicial).value.toUpperCase();
         
         //console.log($nodoInicial.toUpperCase());
 
+        //crea el Set de nodos visitados
         const nodosVisitados = new Set();
 
         //nodosVisitados.add($nodoInicial);
 
+        //crea nuevamente el grafo para aplicar el algoritmoDFS
         const miGrafo = new Grafo();
-        
+
+        //se le agregan los nodos al grafo
         miGrafo.agregarNodo('A');
         miGrafo.agregarNodo('B');
         miGrafo.agregarNodo('C');
         miGrafo.agregarNodo('D');
         miGrafo.agregarNodo('E');
     
+        //se le agregan las aristas al nodo
         miGrafo.agregarArista('A', 'B');
         miGrafo.agregarArista('B', 'C');
         miGrafo.agregarArista('C', 'D');
@@ -131,13 +130,15 @@ export default function recorrerGrafoDFS(btnMostrar, grafo, nodoInicial ,btnReco
         miGrafo.agregarArista('D', 'E');
 
 
-       miGrafo.algoritmoDFS($nodoInicial, nodosVisitados);
+        //se llama a la funcion/metodo algoritmoDFS para recorrer el grafo y que visite todos los nodos
+        miGrafo.algoritmoDFS($nodoInicial, nodosVisitados);
 
 
 
-        console.log(nodosVisitados);
+        //console.log(nodosVisitados);
     });
 
+    //captando evento click del boton Limpiar
     $btnLimpiar.addEventListener("click", () => {
         $grafo.textContent = "";
     });
