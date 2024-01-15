c6b9d2f2551c545d17962f578d4ca56f7c79181b
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-15 16:45:28 -0300

    Resolviendo ejercicio 119

--- ejercicio-119.js
+++ ejercicio-119.js
@@ -5,15 +5,24 @@
 //Además, implementa algún tipo de propagación de eventos.
 
 
-export default function gestionadorDeEventos(btn1, btn2, btn3, btn4){
+export default function gestionadorDeEventos(section ,btn1, btn2, btn3, btn4){
 
+    const $section = document.querySelector(section);
     const $btn1 = document.querySelector(btn1);
     const $btn2 = document.querySelector(btn2);
     const $btn3 = document.querySelector(btn3);
     const $btn4 = document.querySelector(btn4);
 
 
-    
+    $section.addEventListener("click", () => {
+
+
+        
+
+
+
+    });
+
 
 
 }
