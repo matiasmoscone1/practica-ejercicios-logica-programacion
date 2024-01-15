8c1a1646b0775edfe583b67131d8f84c295697f4
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-14 22:01:42 -0300

    Agregando ejercicio 119

---
+++ ejercicio-119.js
@@ -0,0 +1,20 @@
+
+
+//119. Sistema de Gestión de Eventos: Crea un sistema de gestión de eventos personalizado en 
+//JavaScript. Debe permitir la suscripción (o registro), desuscripción y emisión de eventos. 
+//Además, implementa algún tipo de propagación de eventos.
+
+
+export default function gestionadorDeEventos(){
+
+
+
+
+
+    
+}
+
+
+
+
+
