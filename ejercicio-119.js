5569fe33524a0fd3713af4b0ee1bd1a26106052f
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-15 17:46:53 -0300

    Agregando cosas al ej 119

--- ejercicio-119.js
+++ ejercicio-119.js
@@ -3,15 +3,16 @@
 //119. Sistema de Gestión de Eventos: Crea un sistema de gestión de eventos personalizado en 
 //JavaScript. Debe permitir la suscripción (o registro), desuscripción y emisión de eventos. 
 //Además, implementa algún tipo de propagación de eventos.
+//Que cada vez q se suscribe a un evento salten diferentes tipos de eventos
 
 
-export default function gestionadorDeEventos(section ,btn1, btn2, btn3, btn4){
+export default function gestionadorDeEventos(section ,btn1, btn2, btn3, btn4, resultado){
 
     const $section = document.querySelector(section);
+    const $resultado = document.querySelector(resultado);
 
     $section.addEventListener("click", (e) => {
 
-        console.log(e);
 
         if(e.target.matches(btn1)){
             console.log("evento 1");
