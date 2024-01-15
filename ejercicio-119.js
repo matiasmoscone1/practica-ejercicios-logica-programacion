1cfbe62f908e997a06afe225507f95714862e3f0
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-15 16:49:10 -0300

    Realizando ejercicio 119

--- ejercicio-119.js
+++ ejercicio-119.js
@@ -8,21 +8,18 @@
 export default function gestionadorDeEventos(section ,btn1, btn2, btn3, btn4){
 
     const $section = document.querySelector(section);
-    const $btn1 = document.querySelector(btn1);
-    const $btn2 = document.querySelector(btn2);
-    const $btn3 = document.querySelector(btn3);
-    const $btn4 = document.querySelector(btn4);
-
 
     $section.addEventListener("click", (e) => {
 
-        if(e.target.matches($btn1)){
+        console.log(e);
+
+        if(e.target.matches(btn1)){
             console.log("evento 1");
-        }else if(e.target.matches($btn2)){
+        }else if(e.target.matches(btn2)){
             console.log("evento 2");
-        }else if(e.target.matches($btn3)){
+        }else if(e.target.matches(btn3)){
             console.log("evento 3");
-        }else if(e.target.matches($btn4)){
+        }else if(e.target.matches(btn4)){
             console.log("evento 4");
         }
 
