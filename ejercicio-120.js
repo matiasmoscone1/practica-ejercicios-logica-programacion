c5a0e43992b1d8cf1a842c28a26c65a20153f922
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-21 18:48:46 -0300

    Agregando objetos al ejercicio 120

--- ejercicio-120.js
+++ ejercicio-120.js
@@ -7,9 +7,53 @@
 
 export default function algoritmoOrdenamientoPersonalizado(){
 
+    const arrayObjetos = [{
+        nombre: "Teclado",
+        color: "Negro",
+        precio: 1000,
+        marca: "Genius"
+    },
+    {
+        nombre: "Mouse",
+        color: "Rojo",
+        precio: 750,
+        marca: "Genius"
+    },
+    {
+        nombre: "Teclado",
+        color: "Blanco",
+        precio: 1200,
+        marca: "Red Dragon"
+    },
+    {
+        nombre: "Monitor",
+        color: "Negro",
+        precio: 2500,
+        marca: "Samsung"
+    },
+    {
+        nombre: "Mouse",
+        color: "Blanco",
+        precio: 560,
+        marca: "Cx"
+    },
+    {
+        nombre: "Teclado",
+        color: "Negro",
+        precio: 1350,
+        marca: "Red Dragon"
+    },
+    {
+        nombre: "Microfono",
+        color: "Negro",
+        precio: 750,
+        marca: "Logitech"
+    }
+    ];
 
 
 
+    
 
 
 }
