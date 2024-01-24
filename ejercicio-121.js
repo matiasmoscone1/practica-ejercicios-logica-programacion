32423ade1ad21339e3c30ce05878f395f1d13c33
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-24 16:36:14 -0300

    Resolviendo ejercicio 121

--- ejercicio-121.js
+++ ejercicio-121.js
@@ -11,8 +11,21 @@ export default function ejercicioPromesa(btnPromesa, btnLimpiar, resultado){
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
+    const URL = "https://jsonplaceholder.typicode.com/users";
 
-    
+    const callAPI = () => {
+
+        fetch(URL)
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+
+    };
+
+
+    $btnPromesa.addEventListener("click", () => {
+
+        callAPI();
+    });
 
 
 
