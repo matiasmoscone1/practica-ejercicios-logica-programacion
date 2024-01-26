05ffaa1c3c39b3575ffb144b594e97746496647c
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-26 16:36:07 -0300

    Terminando ejercicio 121

--- ejercicio-121.js
+++ ejercicio-121.js
@@ -6,25 +6,33 @@
 
 export default function ejercicioPromesa(btnPromesa, btnLimpiar, resultado){
 
-
+    //creando variables referenciales al DOM
     const $btnPromesa = document.querySelector(btnPromesa);
     const $btnLimpiar = document.querySelector(btnLimpiar);
     const $resultado = document.querySelector(resultado);
 
-    const URL = "https://jsonplacaeholder.typicode.com/users";
+    //creando variable URL que almacenara la url propiamente dicha
+    const URL = "https://jsonplaceholder.typicode.com/users";
 
+    //creando array de usuarios donde se almacenara todos los usuarios
     let arrayUsuarios = [];
 
+    //funcion asincrona que llama a la api y realiza un try catch
     const callAPI = async () => {
 
+        //intenta hacerle una peticion a la api mediante la URL de forma que la funcion no va
+        //a terminar de ejecutarse hasta que este lista esta peticion, luego se concatena .then en
+        //forma de promesas para almacenar todo el contenido de la api en la variable de arrayUsuarios
         try{            
             await fetch(URL)
             .then((response) => response.json())
             .then((data) => {arrayUsuarios = [...data]});
+            //maneja el error
         }catch(e){
             console.error('Nombre:', e.name);
             console.error('Mensaje:', e.message);
             console.error('Seguimiento:', e.stack);
+            //ejecuta siempre el bloque finally
         }finally{
             console.log("Sentencia finalizada...");
         }
