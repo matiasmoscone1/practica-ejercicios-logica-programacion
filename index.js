a203f286061041e84afe0f32bcb9ff76c747fe45
matiasmoscone1 <matiasmoscone1@gmail.com>
2024-01-29 16:44:01 -0300

    Terminando ejercicio 122

--- index.js
+++ index.js
@@ -245,8 +245,8 @@ document.addEventListener("DOMContentLoaded", () => {
     gestionadorDeEventos(".section-119", ".btn1-suscribir-ej-119", ".btn2-suscribir-ej-119", ".btn3-suscribir-ej-119", ".btn4-suscribir-ej-119", ".resultado-eventos-ej-119");
     algoritmoOrdenamientoPersonalizado(".tabla-objetos-ej-120", ".input-ej-120", ".btn-filtrar-ej-120", ".btn-limpiar-ej-120", ".resultado-ej-120");
     ejercicioPromesa(".btn-promesa-ej-121", ".btn-limpiar-ej-121", ".resultado-ej-121");
-    editorTexto();
-    
+    editorTexto(".editor");
+
 
 
 
